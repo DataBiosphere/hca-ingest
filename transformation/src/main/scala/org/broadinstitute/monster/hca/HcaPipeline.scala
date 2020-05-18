ed80c4b562ac7745d244b2396b2091d965a0e984
Raaid Arshad <rarshad@broadinstitute.org>
2020-05-18 14:10:35 -0400

    add newline, fix comment

--- transformation/src/main/scala/org/broadinstitute/monster/hca/HcaPipeline.scala
+++ transformation/src/main/scala/org/broadinstitute/monster/hca/HcaPipeline.scala
@@ -2,7 +2,7 @@ package org.broadinstitute.monster.hca
 
 import org.broadinstitute.monster.common.{PipelineBuilder, ScioApp}
 
-/** Entry-point for the ClinVar pipeline's Docker image. */
+/** Entry-point for the HCA pipeline's Docker image. */
 object HcaPipeline extends ScioApp[Args] {
   override def pipelineBuilder: PipelineBuilder[Args] = HcaPipelineBuilder
 }
