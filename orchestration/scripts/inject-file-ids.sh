4eaaf5c932e2a34c9c13f75abb384d7b89f6bb09
Dan Moran <danmoran@broadinstitute.org>
2020-06-08 17:34:33 -0400

    Fix external schema spec.

--- orchestration/scripts/inject-file-ids.sh
+++ orchestration/scripts/inject-file-ids.sh
@@ -1,10 +1,9 @@
 set -euo pipefail
 
-# Staged rows have: {file_type}_id, version, crc32c, source_file_id, source_file_version, data_file_name
-# Output rows need: {file_type}_id, version, file_id, source_file_id, source_file_version
+# Staged rows have: {file_type}_id, version, crc32c, content, source_file_id, source_file_version, data_file_name
+# Output rows need: {file_type}_id, version, file_id, source_file_id, source_file_version, content
 # Run the transformation by joining on the datarepo_load_history table, via crc32c
 
-declare -r TABLE_SCHEMA=/bq-metadata/${TABLE}/schema.json
 declare -r TARGET_TABLE=${TABLE}_with_ids
 
 declare -ra BQ_QUERY=(
@@ -17,10 +16,10 @@ declare -ra BQ_QUERY=(
   query
   --use_legacy_sql=false
   --replace=true
-  --external_table_definition=${TABLE}::${TABLE_SCHEMA}@NEWLINE_DELIMITED_JSON=${GCS_PREFIX}/*
+  --external_table_definition=${TABLE}::${TABLE}_id:STRING,version:TIMESTAMP,crc32c:STRING,content:STRING,source_file_id:STRING,source_file_version:TIMESTAMP,data_file_name:STRING@NEWLINE_DELIMITED_JSON=${GCS_PREFIX}/*
   --destination_table=${STAGING_PROJECT}:${STAGING_DATASET}.${TARGET_TABLE}
 )
-1>&2 ${BQ_QUERY[@]} "SELECT S.${TABLE}_id, S.version, J.file_id, S.source_file_id, S.source_file_version
+1>&2 ${BQ_QUERY[@]} "SELECT S.${TABLE}_id, S.version, J.file_id, S.source_file_id, S.source_file_version, S.content
   FROM ${TABLE} S LEFT JOIN \`${JADE_PROJECT}.${JADE_DATASET}.${TABLE}\` J
   ON S.crc32c = J.checksum_crc32c"
 
