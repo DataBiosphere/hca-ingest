f0ab7fdd9d96ec7f1b9c76d7aebc7489d3abb56f
Sarah Wessel <swessel@broadinstitute.org>
2020-08-12 10:36:57 -0400

    Remove 'if' from bash script so we know when it fails

--- orchestration/scripts/move-error-logs-to-gcs.sh
+++ orchestration/scripts/move-error-logs-to-gcs.sh
@@ -4,11 +4,9 @@ declare -r LOGS_DIR=../../logs
 declare -r LOG_FILE=${LOGS_DIR}/errors.log
 declare -r NEW_FILENAME=${LOGS_DIR}/${TIMESTAMP}.log
 
-if test -f "$LOG_FILE"; then
-  # rename the file to the start time of the import
-  mv ${LOG_FILE} ${NEW_FILENAME}
-  # move the file to a bucket
-  gsutil cp ${NEW_FILENAME} ${BUCKET_NAME}/errors/
-  rm ${NEW_FILENAME}
-fi
+# rename the file to the start time of the import
+mv ${LOG_FILE} ${NEW_FILENAME}
+# move the file to a bucket
+gsutil cp ${NEW_FILENAME} ${BUCKET_NAME}/errors/
+rm ${NEW_FILENAME}
 
