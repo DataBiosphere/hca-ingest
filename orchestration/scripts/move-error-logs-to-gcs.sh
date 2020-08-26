80b96494ae75168c66a9cb87fba2b1e76919230e
Sarah Wessel <swessel@broadinstitute.org>
2020-08-26 10:12:24 -0400

    Add script (moved from other PR)

---
+++ orchestration/scripts/move-error-logs-to-gcs.sh
@@ -0,0 +1,11 @@
+set -euo pipefail
+
+declare -r LOGS_DIR=../../logs
+declare -r LOG_FILE=${LOGS_DIR}/errors.log
+declare -r NEW_FILENAME=${LOGS_DIR}/${TIMESTAMP}.log
+
+# rename the file to the start time of the import
+mv ${LOG_FILE} ${NEW_FILENAME}
+# move the file to a bucket
+gsutil cp ${NEW_FILENAME} ${BUCKET_NAME}/errors/
+rm ${NEW_FILENAME}
