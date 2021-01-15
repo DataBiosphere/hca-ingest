fc2b62c6ce5d5752c9c167f4ad6f81c9096e4103
Raaid Arshad <rarshad@broadinstitute.org>
2021-01-15 14:33:07 -0500

    nonsensical but successful demonstration of how to fan out/fan in

--- orchestration/dagster_poc/hca_proto/pipelines.py
+++ orchestration/dagster_poc/hca_proto/pipelines.py
@@ -21,7 +21,16 @@ dev_mode = ModeDefinition(
     mode_defs=[prod_mode, dev_mode]
 )
 def stage_data():
-    submit_file_ingest(pre_process_metadata(clear_staging_dir()))
+    middle = pre_process_metadata(clear_staging_dir())
+    entities = ["analysis_file", "analysis_process", "analysis_protocol"]
+
+    outs = []
+    for e in entities:
+        submit = submit_file_ingest.alias(e)
+        outs.append(submit(middle))
+
+    final = submit_file_ingest.alias("final")
+    final(outs)
 
 
 @repository
