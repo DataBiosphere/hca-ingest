12344c3f9084a44b99795043b42a647633edcfc8
Raaid Arshad <rarshad@broadinstitute.org>
2021-01-27 14:24:58 -0500

    initialize boiletplate/skeleton

---
+++ orchestration/prefect_poc/hca-proto/hca_proto/main.py
@@ -0,0 +1,16 @@
+
+
+def run_dataflow_job():
+    pass
+
+
+def enumerate_jade_datasets():
+    pass
+
+
+def dummy_fan_out():
+    pass
+
+
+def dummy_fan_in():
+    pass
---
+++ orchestration/prefect_poc/hca-proto/tests/test_hca_proto.py
@@ -0,0 +1,5 @@
+from hca_proto import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
