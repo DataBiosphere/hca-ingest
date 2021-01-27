b2554b08f68b9db96456e68d434250d7543a5aef
Raaid Arshad <rarshad@broadinstitute.org>
2021-01-27 14:27:08 -0500

    add basic comments for what we need to show in this poc

--- orchestration/prefect_poc/hca-proto/hca_proto/main.py
+++ orchestration/prefect_poc/hca-proto/hca_proto/main.py
@@ -1,16 +1,25 @@
 
 
+# show interaction with GCS
+def clear_staging_area():
+    pass
+
+
+# show ability to run container in k8s
 def run_dataflow_job():
     pass
 
 
+# show ability to communicate with jade
 def enumerate_jade_datasets():
     pass
 
 
+# show ability to fan out
 def dummy_fan_out():
     pass
 
 
+# show ability to fan back in
 def dummy_fan_in():
     pass
