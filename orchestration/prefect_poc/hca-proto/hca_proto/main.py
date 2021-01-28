5eef61746ca8d750fb87f7266b1ce0d86c7067ff
Raaid Arshad <rarshad@broadinstitute.org>
2021-01-28 10:31:08 -0500

    add tasks to clear staging area and hit enumerate datasets jade endpoint

--- orchestration/prefect_poc/hca-proto/hca_proto/main.py
+++ orchestration/prefect_poc/hca-proto/hca_proto/main.py
@@ -1,8 +1,23 @@
+from data_repo_client import ApiClient, Configuration, RepositoryApi, EnumerateDatasetModel
+import google.auth
+from google.auth.transport.requests import Request
+from google.cloud import storage
+import prefect
+from prefect import task
 
 
-# show interaction with GCS
-def clear_staging_area():
-    pass
+@task
+def clear_staging_area(bucket_name: str, blob_name: str):
+    credentials, project = google.auth.default()
+
+    storage_client = storage.Client(project=project, credentials=credentials)
+
+    blobs = storage_client.list_blobs(bucket_name, prefix=f"{blob_name}/")
+    count = 0
+    for count, blob in enumerate(blobs):
+        blob.delete()
+    logger = prefect.context.get("logger")
+    logger.debug(f"--clear_staging_dir found {count} blobs to delete under {blob_name}")
 
 
 # show ability to run container in k8s
@@ -10,9 +25,26 @@ def run_dataflow_job():
     pass
 
 
-# show ability to communicate with jade
+@task
 def enumerate_jade_datasets():
-    pass
+    # get token for jade, assumes application default credentials work for specified environment
+    credentials, _ = google.auth.default()
+    auth_req = Request()
+    credentials.refresh(auth_req)
+
+    # create API client
+    config = Configuration(host="https://jade.datarepo-dev.broadinstitute.org/")
+    config.access_token = credentials.token
+    client = ApiClient(configuration=config)
+    client.client_side_validation = False
+
+    # submit file ingest (for now just enumerate datasets or something to prove interaction works)
+    repoApi = RepositoryApi(api_client=client)
+
+    datasets = repoApi.enumerate_datasets()
+
+    logger = prefect.context.get("logger")
+    logger.debug(f"Enumerate found {datasets.total} datasets in the repo")
 
 
 # show ability to fan out
