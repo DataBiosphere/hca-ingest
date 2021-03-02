8680e0105733d66bbcaaa4c111f7f50600e2844c
Steve Pletcher <spletche@broadinstitute.org>
2021-03-02 15:29:23 -0500

    fill out type annotations

--- orchestration/dagster_orchestration/hca_orchestration/contrib/argo_workflows.py
+++ orchestration/dagster_orchestration/hca_orchestration/contrib/argo_workflows.py
@@ -1,10 +1,14 @@
+from __future__ import annotations  # this lets us annotate functions in class C that return an instance of C
+
+from typing import Any, Callable, Dict, Generator, Optional
+
 from argo.workflows.client import ApiClient as ArgoApiClient,\
                                   ArchivedWorkflowServiceApi,\
                                   Configuration as ArgoConfiguration
-from argo.workflows.client.models import V1alpha1Workflow
+from argo.workflows.client.models import V1alpha1Workflow, V1alpha1WorkflowList
 
 
-def generate_argo_archived_workflows_client(host_url, access_token):
+def generate_argo_archived_workflows_client(host_url: str, access_token: str) -> ArchivedWorkflowServiceApi:
     return ArchivedWorkflowServiceApi(
         api_client=ArgoApiClient(
             configuration=ArgoConfiguration(host=host_url),
@@ -13,22 +17,22 @@ def generate_argo_archived_workflows_client(host_url, access_token):
 
 
 class ArgoArchivedWorkflowsClientMixin:
-    def __init__(self, *args, argo_url, access_token, **kwargs):
+    def __init__(self, *args, argo_url: str, access_token: str, **kwargs):
         super().__init__(*args, **kwargs)
         self.argo_url = argo_url
         self.access_token = access_token
         self._client = None
 
-    def client(self):
+    def client(self) -> ArchivedWorkflowServiceApi:
         if not self._client:
             self._client = generate_argo_archived_workflows_client(self.argo_url, self.access_token)
 
         return self._client
 
-    def list_archived_workflows(self):
-        self._pull_paginated_results(self.client().list_archived_workflows)
+    def list_archived_workflows(self) -> Generator[V1alpha1Workflow, None, None]:
+        return self._pull_paginated_results(self.client().list_archived_workflows)
 
-    def _pull_paginated_results(self, api_function):
+    def _pull_paginated_results(self, api_function: Callable[[Optional[str]], V1alpha1WorkflowList]) -> Generator[V1alpha1Workflow, None, None]:
         results = api_function()
 
         for result in results.items:
@@ -47,19 +51,20 @@ class ExtendedArgoWorkflow(ArgoArchivedWorkflowsClientMixin):
         self._workflow = workflow
         self._inflated = False
 
-    def inflate(self):
+    def inflate(self) -> ExtendedArgoWorkflow:
         if not self._inflated:
             self._workflow = self.client().get_archived_workflow(self.metadata.uid)
             self._inflated = True
 
         return self
 
-    def params_dict(self):
+    def params_dict(self) -> Dict[str, Any]:
         return {
             param.name: param.value
             for param in self.spec.arguments.parameters
         }
 
+    # proxy pattern - any function calls not defined in this class are passed to the wrapped workflow object
     def __getattr__(self, name):
         return getattr(self._workflow, name)
 
