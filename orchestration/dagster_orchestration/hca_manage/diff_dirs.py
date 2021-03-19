360153b335ccb0ff40cd488094cb55f698fc8c51
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-03-19 09:21:41 -0400

    [DSPDC-952] Run test transformation pipeline against updated dataset (#80)
    Why
    
    We have a test dataset provided by EBI. I have modified it to remove most of the data files and get a representative test case for import. We need to run our dataflow check against this dataset and check it against a set of known good outputs.
    
    This PR
    
    * Placed the "slimmed" dataset into our test storage bucket at gs://broad-dsp-monster-hca-dev-test-storage/integration/ebi_small
    * Placed the output of a dataflow run at gs://broad-dsp-monster-hca-dev-test-storage/integration/ebi_small/expected_output.
    * Adds a simple script to diff the output of a test dataflow run and the expected output dir. As we modify the transformation pipeline (i.e., to accommodate schema changes), I expect the flow to be something like:
      * Modify transformation pipeline and ensure unit tests run
      * Test in dataflow and get output
      * 👁️ the output and if good, put it in the ebi_small/expected_output directory
    * I have not modified the validate-pull-request.yaml action to keep the scope of this PR down and to let us test out this testing mechanism before introducing it more broadly.

---
+++ orchestration/dagster_orchestration/hca_manage/diff_dirs.py
@@ -0,0 +1,41 @@
+"""
+Compares the contents of two google storage paths via their md5
+"""
+import argparse
+import logging
+
+import google.auth
+from google.cloud import storage
+
+logging.basicConfig(level=logging.INFO)
+
+
+def run(project, source_bucket, source_prefix, target_bucket, target_prefix):
+    creds, _ = google.auth.default()
+    storage_client = storage.Client(project=project, credentials=creds)
+    expected_blobs = {blob.name.replace(source_prefix, ''): blob.md5_hash
+                      for blob in storage_client.list_blobs(source_bucket,
+                                                            prefix=source_prefix)}
+
+    logging.info(f"project = {project}, "
+                 f"source_bucket = {source_bucket}, "
+                 f"source_prefix = {source_prefix}, "
+                 f"target_bucket = {target_bucket}, "
+                 f"target_prefix = {target_prefix}")
+    output_blobs = {blob.name.replace(target_prefix, ''): blob.md5_hash
+                    for blob in storage_client.list_blobs(target_bucket,
+                                                          prefix=target_prefix)}
+
+    assert expected_blobs == output_blobs, "Output results differ from expected"
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--project")
+    parser.add_argument("-sb", "--source_bucket")
+    parser.add_argument("-sp", "--source_prefix")
+
+    parser.add_argument("-tb", "--target_bucket")
+    parser.add_argument("-tp", "--target_prefix")
+    args = parser.parse_args()
+    run(args.project, args.source_bucket, args.source_prefix, args.target_bucket, args.target_prefix)
