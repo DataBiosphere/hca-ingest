1995246f2ee42a4654bc0571c1014a2c4d0fbb29
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-03-30 16:50:23 -0400

    E2E stage-data pipeline test with real resources (#90)
    Why
    
    We'd like to be able to run the stage-data pipeline against real test data with a real beam runner.
    
    This PR
    
    Adds a test_stage_data_local_e2e test that runs the stage-data pipeline against our ebi_small dataset using a local beam runner.
    Adds a e2e pytest marker so we can selectively exclude this test. It's pretty quick locally, but there are likely scenarios where we don't want to reach out to GS when testing.

--- orchestration/dagster_orchestration/hca_manage/diff_dirs.py
+++ orchestration/dagster_orchestration/hca_manage/diff_dirs.py
@@ -12,7 +12,13 @@ from hca_orchestration.contrib import google as hca_google
 logging.basicConfig(level=logging.INFO)
 
 
-def run(project: str, source_bucket: str, source_prefix: str, target_bucket: str, target_prefix: str) -> None:
+def diff_dirs(
+    project: str,
+    source_bucket: str,
+    source_prefix: str,
+    target_bucket: str,
+    target_prefix: str
+) -> tuple[dict[str, int], dict[str, int]]:
     creds = hca_google.get_credentials()
     storage_client = storage.Client(project=project, credentials=creds)
     expected_blobs = {blob.name.replace(source_prefix, ''): blob.md5_hash
@@ -28,7 +34,7 @@ def run(project: str, source_bucket: str, source_prefix: str, target_bucket: str
                     for blob in storage_client.list_blobs(target_bucket,
                                                           prefix=target_prefix)}
 
-    assert expected_blobs == output_blobs, "Output results differ from expected"
+    return expected_blobs, output_blobs
 
 
 if __name__ == '__main__':
@@ -40,4 +46,6 @@ if __name__ == '__main__':
     parser.add_argument("-tb", "--target_bucket")
     parser.add_argument("-tp", "--target_prefix")
     args = parser.parse_args()
-    run(args.project, args.source_bucket, args.source_prefix, args.target_bucket, args.target_prefix)
+    expected, output = diff_dirs(args.project, args.source_bucket, args.source_prefix,
+                                 args.target_bucket, args.target_prefix)
+    assert expected == output, "Output results differ from expected"
--- orchestration/dagster_orchestration/hca_orchestration/tests/test_pipelines.py
+++ orchestration/dagster_orchestration/hca_orchestration/tests/test_pipelines.py
@@ -1,37 +1,88 @@
+import os
+import pytest
 import unittest
 from unittest.mock import patch
+import uuid
+from typing import Any
 
-import os
-from dagster import execute_pipeline, file_relative_path
+
+from dagster import execute_pipeline, file_relative_path, PipelineDefinition, PipelineExecutionResult
 from dagster.utils import load_yaml_from_globs
+from dagster.utils.merger import deep_merge_dicts
 from hca_orchestration.pipelines import stage_data, validate_egress
+from hca_manage.diff_dirs import diff_dirs
+from hca_orchestration.support.typing import DagsterConfigDict
 
 
-def config_path(relative_path):
-    return file_relative_path(
+def config_path(relative_path: str) -> str:
+    path: str = file_relative_path(
         __file__, os.path.join("../environments/", relative_path)
     )
+    return path
+
+
+def beam_runner_path() -> str:
+    path: str = file_relative_path(__file__, '../../../../')
+    return path
 
 
 class PipelinesTestCase(unittest.TestCase):
-    def run_pipeline(self, pipeline, config_name, *execution_args, **execution_kwargs):
+    def run_pipeline(
+            self,
+            pipeline: PipelineDefinition,
+            config_name: str,
+            extra_config: dict[str, Any] = {},
+        pipeline_mode='test'
+    ) -> PipelineExecutionResult:
         config_dict = load_yaml_from_globs(
             config_path(config_name)
         )
+        config_dict = deep_merge_dicts(config_dict, extra_config)
+
         return execute_pipeline(
             pipeline,
-            *execution_args,
-            mode='test',
             run_config=config_dict,
-            **execution_kwargs
+            mode=pipeline_mode
         )
 
-    def test_stage_data(self):
-        """
-        Simple example of running a pipeline with "noop" resources
-        TODO Build a 'real' E2E pipeline invocation that runs
-        against GS and a local Beam runner
-        """
+    @pytest.mark.e2e
+    def test_stage_data_local_e2e(self):
+        test_id = f'test-{uuid.uuid4()}'
+        config = load_yaml_from_globs(config_path('stage_data_local_e2e.yaml'))
+        runtime_config = {
+            'resources': {
+                'beam_runner': {
+                    'config': {
+                        'working_dir': beam_runner_path()
+                    }
+                }
+            },
+            'solids': {
+                'clear_staging_dir': {
+                    'config': {
+                        'staging_prefix_name': f'local-stage-data/{test_id}'
+                    }
+                },
+                'pre_process_metadata': {
+                    'config': {
+                        'staging_prefix_name': f'local-stage-data/{test_id}'
+                    }
+                }
+            }
+        }
+
+        self.run_pipeline(stage_data, 'stage_data_local_e2e.yaml', extra_config=runtime_config, pipeline_mode='local')
+
+        expected_blobs, output_blobs = diff_dirs(
+            'broad-dsp-monster-hca-dev',
+            'broad-dsp-monster-hca-dev-test-storage',
+            'integration/ebi_small/expected_output',
+            'broad-dsp-monster-hca-dev-temp-storage',
+            f'local-stage-data/{test_id}',
+        )
+        self.assertEqual(expected_blobs, output_blobs, "Output results differ from expected")
+
+    def test_stage_data_noop_resources(self):
         result = self.run_pipeline(stage_data, config_name="test_stage_data.yaml")
 
         self.assertTrue(result.success)
@@ -47,7 +98,6 @@ class PipelinesTestCase(unittest.TestCase):
         post_import_validate, so this just spins it up and sees if
         it runs at all
         """
-
         result = self.run_pipeline(validate_egress, config_name="test_validate_egress.yaml")
 
         self.assertTrue(result.success)
