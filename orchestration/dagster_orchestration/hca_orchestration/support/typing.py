2ea893969617767228553552d4a991b0de92fe68
Steve Pletcher <spletche@broadinstitute.org>
2021-03-30 12:04:54 -0400

    More strictly define valid dagster config dicts

--- orchestration/dagster_orchestration/hca_orchestration/support/typing.py
+++ orchestration/dagster_orchestration/hca_orchestration/support/typing.py
@@ -1,7 +1,18 @@
 # Complex type signatures that appear multiple times throughout the code base can live here,
 # for easy reference and descriptive naming.
 
-from typing import Union
+from typing import Literal, Union
 
 
-DagsterConfigDict = dict[str, Union[dict[str, str], str]]
+DagsterConfigDict = dict[
+    str,
+    Union[
+        dict[
+            Literal['env'],
+            str
+        ],
+        str,
+        int,
+        float
+    ]
+]
