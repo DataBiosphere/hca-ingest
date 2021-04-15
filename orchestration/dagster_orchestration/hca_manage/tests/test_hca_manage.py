d45aaa6df3cfdc2e1489abdba74c738def9b17c7
Steve Pletcher <spletche@broadinstitute.org>
2021-04-15 09:33:23 -0400

    break up tests for readability

--- orchestration/dagster_orchestration/hca_manage/tests/test_hca_manage.py
+++ orchestration/dagster_orchestration/hca_manage/tests/test_hca_manage.py
@@ -33,7 +33,9 @@ class HcaManageTestCase(unittest.TestCase):
     def test_populate_row_id_csv_writes_to_csv(self):
         string_io = StringIO()
         strings = {'abc', 'def', 'ghi'}
+
         HcaManage.populate_row_id_csv(strings, string_io)
+
         # move to the start so we can read the newly-written text
         string_io.seek(0)
         csv_reader = csv.reader(string_io)
@@ -43,11 +45,13 @@ class HcaManageTestCase(unittest.TestCase):
         fake_gcs_client = FakeGCSClient()
         target_bucket = self.manager.bucket
         expected_filename = self.manager._format_filename('some_table')
+
         with patch('google.cloud.storage.Client', return_value=fake_gcs_client):
             very_real_csv = BytesIO(b"steve\r\nwas\r\nhere\r\n")
             self.manager.put_csv_in_bucket(very_real_csv, 'some_table')
-            expected_target_blob = fake_gcs_client.get_bucket(target_bucket).blob(expected_filename)
-            expected_target_blob.upload_from_file.assert_called_once_with(very_real_csv)
+
+        expected_target_blob = fake_gcs_client.get_bucket(target_bucket).blob(expected_filename)
+        expected_target_blob.upload_from_file.assert_called_once_with(very_real_csv)
 
     def test_process_rows_returns_zero_if_no_bad_rids(self):
         result = self.manager._process_rows(
@@ -55,6 +59,7 @@ class HcaManageTestCase(unittest.TestCase):
             lambda table: set(),
             soft_delete=True,
             issue="oh dear")
+
         self.assertEqual(result, 0)
 
     def test_process_rows_does_no_soft_delete_if_soft_delete_false(self):
@@ -64,6 +69,7 @@ class HcaManageTestCase(unittest.TestCase):
                 lambda table: {'abc'},
                 soft_delete=False,
                 issue="oh dear")
+
             mock_soft_delete.assert_not_called()
         self.assertEqual(result, 2)  # one error per table
 
@@ -74,6 +80,7 @@ class HcaManageTestCase(unittest.TestCase):
                 lambda table: {'abc'},
                 soft_delete=True,
                 issue="oh dear")
+
             mock_soft_delete.assert_has_calls(
                 [
                     call(StringContaining('table_a.csv'), 'table_a'),
@@ -88,9 +95,11 @@ class HcaManageTestCase(unittest.TestCase):
         fake_job_id = 'jorb_id'
         patched_soft_delete = patch('hca_manage.manage.HcaManage._submit_soft_delete', return_value=fake_job_id)
         patched_put_in_bucket = patch('hca_manage.manage.HcaManage.put_csv_in_bucket', return_value=fake_google_path)
+
         with patched_soft_delete as mock_submit, patched_put_in_bucket as mock_bucket_upload:
             with NamedTemporaryFile() as fake_file:
                 returned_job_id = self.manager.soft_delete_rows(fake_file.name, 'table_a')
+
             mock_bucket_upload.assert_called_once_with(
                 local_file=ObjectOfType(BufferedIOBase),
                 target_table='table_a',
@@ -102,11 +111,11 @@ class HcaManageTestCase(unittest.TestCase):
         enumerate_datasets_response = Mock()
         first_dataset = Mock()
         first_dataset.id = 'abc'
-
         second_dataset = Mock()
         second_dataset.id = 'def'
         enumerate_datasets_response.items = [first_dataset, second_dataset]
         self.manager.data_repo_client.enumerate_datasets.return_value = enumerate_datasets_response
+
         self.assertEqual(self.manager.get_dataset_id(), 'abc')
 
     def test_submit_soft_delete_submits_dataset_deletion_with_expected_params(self):
@@ -115,7 +124,9 @@ class HcaManageTestCase(unittest.TestCase):
         single_dataset.id = 'abc'
         enumerate_datasets_response.items = [single_dataset]
         self.manager.data_repo_client.enumerate_datasets.return_value = enumerate_datasets_response
+
         self.manager._submit_soft_delete('table_a', 'bad_rows_a.csv')
+
         self.manager.data_repo_client.apply_dataset_data_deletion.assert_called_once_with(
             id='abc',
             data_deletion_request=DataDeletionRequest(
@@ -135,6 +146,7 @@ class HcaManageTestCase(unittest.TestCase):
 
     def test_submit_snapshot_request_ignores_qualifier_if_not_present(self):
         self.manager.submit_snapshot_request(qualifier=None)
+
         self.manager.data_repo_client.create_snapshot.assert_called_once_with(
             snapshot=ObjectWithAttributes(
                 SnapshotRequestModel,
@@ -143,6 +155,7 @@ class HcaManageTestCase(unittest.TestCase):
 
     def test_submit_snapshot_request_uses_qualifier_if_present(self):
         self.manager.submit_snapshot_request(qualifier='steve')
+
         self.manager.data_repo_client.create_snapshot.assert_called_once_with(
             snapshot=ObjectWithAttributes(
                 SnapshotRequestModel,
@@ -155,11 +168,14 @@ class HcaManageTestCase(unittest.TestCase):
         single_snapshot.id = 'abc'
         enumerate_snapshots_response.items = [single_snapshot]
         self.manager.data_repo_client.enumerate_snapshots.return_value = enumerate_snapshots_response
+
         self.manager.delete_snapshot(snapshot_name='steve')
+
         self.manager.data_repo_client.delete_snapshot.assert_called_once_with('abc')
 
     def test_delete_snapshot_uses_id_if_provided(self):
         self.manager.delete_snapshot(snapshot_id='steve')
+
         self.manager.data_repo_client.delete_snapshot.assert_called_once_with('steve')
 
     def test_delete_snapshot_blows_up_if_both_or_neither_id_and_name(self):
@@ -175,11 +191,14 @@ class HcaManageTestCase(unittest.TestCase):
         single_dataset.id = 'abc'
         enumerate_datasets_response.items = [single_dataset]
         self.manager.data_repo_client.enumerate_datasets.return_value = enumerate_datasets_response
+
         self.manager.delete_dataset(dataset_name='steve')
+
         self.manager.data_repo_client.delete_dataset.assert_called_once_with('abc')
 
     def test_delete_dataset_uses_id_if_provided(self):
         self.manager.delete_dataset(dataset_id='steve')
+
         self.manager.data_repo_client.delete_dataset.assert_called_once_with('steve')
 
     def test_delete_dataset_blows_up_if_both_or_neither_id_and_name(self):
