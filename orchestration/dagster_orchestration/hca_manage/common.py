bd15bee1bbf2c4cf9fd1ab4c49872fe6f4b6833b
Raaid Arshad <rarshad@broadinstitute.org>
2021-04-20 09:41:16 -0400

    create dataset-specific script, start moving code to a common.py file

---
+++ orchestration/dagster_orchestration/hca_manage/common.py
@@ -0,0 +1,64 @@
+import argparse
+import sys
+from typing import NoReturn
+
+from data_repo_client import ApiClient, Configuration, RepositoryApi
+
+from hca_orchestration.contrib.google import default_google_access_token
+
+
+data_repo_host = {
+    "dev": "https://jade.datarepo-dev.broadinstitute.org/",
+    "prod": "https://jade-terra.datarepo-prod.broadinstitute.org/"
+}
+
+
+class DefaultHelpParser(argparse.ArgumentParser):
+    def error(self, message: str) -> NoReturn:
+        """Print help message by default."""
+        sys.stderr.write(f'error: {message}\n')
+        self.print_help()
+        sys.exit(2)
+
+
+def get_api_client(host: str) -> RepositoryApi:
+    # create API client
+    config = Configuration(host=host)
+    config.access_token = default_google_access_token()
+    client = ApiClient(configuration=config)
+    client.client_side_validation = False
+
+    return RepositoryApi(api_client=client)
+
+
+def query_yes_no(question: str, default: str = "no") -> bool:
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError(f"invalid default answer: '{default}'")
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
---
+++ orchestration/dagster_orchestration/hca_manage/dataset.py
@@ -0,0 +1,53 @@
+import argparse
+from typing import List, Optional
+
+from hca_manage import __version__ as hca_manage_version
+from hca_manage.common import data_repo_host, DefaultHelpParser, get_api_client, query_yes_no
+from hca_manage.manage import HcaManage, JobId
+
+
+def run(arguments: Optional[List[str]] = None) -> None:
+    parser = DefaultHelpParser(description="A simple CLI to manage TDR datasets.")
+    parser.add_argument("-V", "--version", action="version", version="%(prog)s " + hca_manage_version)
+    parser.add_argument("-e", "--env", help="The Jade environment to target", choices=["dev", "prod"], required=True)
+
+    dataset_flags = parser.add_mutually_exclusive_group(required=True)
+    dataset_flags.add_argument("-c", "--create", help="Flag to create a dataset", action="store_true")
+    dataset_flags.add_argument("-r", "--remove", help="Flag to delete a dataset", action="store_true")
+
+    # create
+    dataset_create_args = parser.add_argument_group()
+    dataset_create_args.add_argument("-b", "--billing_profile_id", help="Billing profile ID")
+    dataset_create_args.add_argument("-j", "--schema_path", help="Path to table schema (JSON)", required=True)
+
+    # delete
+    dataset_delete_args = parser.add_mutually_exclusive_group(required=True)
+    dataset_delete_args.add_argument("-n", "--dataset_name", help="Name of dataset to delete.")
+    dataset_delete_args.add_argument("-i", "--dataset_id", help="ID of dataset to delete.")
+
+    args = parser.parse_args(arguments)
+    host = data_repo_host[args.env]
+
+    if args.remove:
+        if query_yes_no("Are you sure?"):
+            remove_dataset(args, host)
+        else:
+            # TODO change to logger?
+            print("No deletion attempted.")
+    elif args.create:
+        if query_yes_no("This will create a dataset. Are you sure?"):
+            create_dataset(args, host)
+
+
+def remove_dataset(args: argparse.Namespace, host: str) -> JobId:
+    hca = HcaManage(environment=args.env, data_repo_client=get_api_client(host=host))
+    return hca.delete_dataset(dataset_name=args.dataset_name, dataset_id=args.dataset_id)
+
+
+def create_dataset(args: argparse.Namespace, host: str) -> JobId:
+    hca = HcaManage(environment=args.env, data_repo_client=get_api_client(host=host))
+    return hca.create_dataset(
+        dataset_name=args.dataset_name,
+        billing_profile_id=args.billing_profile_id,
+        schema_path=args.schema_path
+    )
