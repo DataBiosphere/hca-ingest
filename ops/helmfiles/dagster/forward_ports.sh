0ccf00c94ecbe651b65868f461c15b6d2a43cc00
Steve Pletcher <steve@steve-pletcher.com>
2021-04-28 16:00:45 -0400

    Add default scopes to GCloud tokens (#108)
    * Add default scopes to GCloud tokens
    
    * disable scala stuff
    
    * undo temp deploy changes
    
    * FOUR scopes

--- ops/helmfiles/dagster/forward_ports.sh
+++ ops/helmfiles/dagster/forward_ports.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # This script forwards port 8080 to the Kubernetes cluster running our Dagster install so you can access
 # the Dagit dashboard from your machine.
+ENV=${1:-dev}
+
+echo $ENV
+
+if [ "$ENV" == "prod" ]; then
+	gcloud container clusters get-credentials hca-cluster --project mystical-slate-284720 --region us-central1-c
+else
+	gcloud container clusters get-credentials hca-cluster --project broad-dsp-monster-hca-dev --region us-central1-c
+fi
+
 export DAGIT_POD_NAME=$(kubectl get pods --namespace dagster -l "app.kubernetes.io/name=dagster,app.kubernetes.io/instance=monster,component=dagit" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace dagster port-forward $DAGIT_POD_NAME 8080:80
