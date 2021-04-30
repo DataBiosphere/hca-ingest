c5c079e3092b6e461f56ea8e0cea5c292ef7eaae
Drew Herbst <aherbst@broadinstitute.org>
2021-04-30 11:36:52 -0400

    Update verifier to find a loaded file over all time ranges in the repo, not just the latest

--- orchestration/dagster_orchestration/hca_manage/verify_release_manifest.py
+++ orchestration/dagster_orchestration/hca_manage/verify_release_manifest.py
@@ -10,9 +10,12 @@ python verify_release_manifest.py -s 2021-03-24 -f testing.csv -g fake-gs-projec
 """
 import argparse
 import logging
+from functools import partial
+
 import sys
 from urllib.parse import urlparse
 from collections import defaultdict
+from multiprocessing import Pool
 
 from google.cloud import bigquery, storage
 from google.cloud.bigquery.table import Row
@@ -21,7 +24,6 @@ from hca_orchestration.contrib import google as hca_google
 import json
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
-
 
 file_types = {
     'analysis_file',
@@ -33,38 +35,35 @@ file_types = {
 }
 
 
-def get_expected_load_totals(storage_client: Client, staging_areas: list[str]) -> dict[str, list[str]]:
+def get_expected_load_totals(storage_client: Client, staging_areas: set[str]) -> dict[str, set[str]]:
     """
     Given a list of GS staging areas, count the files present in each /data subdir
     """
 
-    expected = defaultdict(list[str])
+    expected = defaultdict(set[str])
     for staging_area in staging_areas:
         url = urlparse(staging_area)
         for file_type in file_types:
             prefix = f"{url.path.lstrip('/')}/descriptors/{file_type}"
             blobs = list(storage_client.list_blobs(url.netloc, prefix=prefix))
-            logging.info("Pulling descriptors...")
             for blob in blobs:
                 descriptor = json.loads(blob.download_as_text())
                 target_path = f"/{descriptor['file_id']}/{descriptor['file_name']}"
 
-                expected[staging_area].append(target_path)
+                expected[staging_area].add(target_path)
     return expected
 
 
-def find_files_in_load_history(bq_project: str, dataset: str, areas: list[str], start_date: str):
-    import pdb
-    pdb.set_trace()
+def find_files_in_load_history(bq_project: str, dataset: str, areas: dict[str, set]):
     client = bigquery.Client(project=bq_project)
-
+    loaded_paths = defaultdict(set[str])
     for area, target_paths in areas.items():
+        logging.debug(f"\tPulling loaded files for area {area}...")
         query = f"""
             SELECT distinct(target_path)
                         FROM `datarepo_{dataset}.datarepo_load_history` dlh
-                        WHERE load_time >= '{start_date}'
-                        and state = 'succeeded'
-                        and target_path IN (@paths)
+                        WHERE  state = 'succeeded'
+                        and target_path IN UNNEST(@paths)
         """
         job_config = bigquery.QueryJobConfig(
             query_parameters=[
@@ -72,44 +71,8 @@ def find_files_in_load_history(bq_project: str, dataset: str, areas: list[str], 
             ]
         )
         query_job = client.query(query, job_config=job_config)
-        import pdb
-        pdb.set_trace()
-        return [row for row in query_job]
-
-
-def get_load_history(bq_project: str, dataset: str, start_date: str) -> list[Row]:
-    client = bigquery.Client(project=bq_project)
-
-    # Determine the number of distinct files loaded after the given start date, grouped by staging area
-    query = f"""
-                WITH base as (
-                    SELECT distinct(target_path)
-                    FROM `datarepo_{dataset}.datarepo_load_history` dlh
-                    LEFT JOIN `datarepo_{dataset}.sequence_file` sf
-                    ON sf.file_id = dlh.file_id
-                    LEFT JOIN `datarepo_{dataset}.analysis_file` af
-                    ON af.file_id = dlh.file_id
-                    LEFT JOIN `datarepo_{dataset}.reference_file` rf
-                    ON rf.file_id = dlh.file_id
-                    LEFT JOIN `datarepo_{dataset}.supplementary_file` supf
-                    ON supf.file_id = dlh.file_id
-                    LEFT JOIN `datarepo_{dataset}.image_file` imgf
-                    ON imgf.file_id = dlh.file_id
-                    WHERE load_time >= '{start_date}'
-                    AND state = 'succeeded'
-                    AND (sf.file_id IS NOT NULL OR
-                        af.file_id IS NOT NULL OR
-                        rf.file_id IS NOT NULL OR
-                        supf.file_id IS NOT NULL OR
-                        imgf.file_id IS NOT NULL)
-                )
-                SELECT REGEXP_EXTRACT(source_name, '(gs://.*)/data/.*$') AS bucket, count(*) as cnt
-                FROM base GROUP by bucket;
-                """
-    query_job = client.query(query)
-
-    # hydrate the rows
-    return [row for row in query_job]
+        loaded_paths[area] = {row[0] for row in query_job}
+    return loaded_paths
 
 
 def parse_manifest_file(manifest_file: str) -> list[str]:
@@ -118,58 +81,46 @@ def parse_manifest_file(manifest_file: str) -> list[str]:
         return [area.rstrip('\n/') for area in manifest]
 
 
-def verify(start_date: str, manifest_file: str, gs_project: str, bq_project: str, dataset: str) -> bool:
-    logging.info("Parsing manifest and inspecting staging areas...")
+def process_staging_area(area: str, gs_project: str, bq_project: str, dataset: str, ):
+    logging.debug(f"Processing staging area = {area}")
     creds = hca_google.get_credentials()
     storage_client = storage.Client(project=gs_project, credentials=creds)
-    staging_areas = parse_manifest_file(manifest_file)
-    expected_load_totals = get_expected_load_totals(storage_client, staging_areas)
-    find_files_in_load_history(bq_project, dataset, expected_load_totals, start_date)
-    sys.exit(1)
+    expected_load_totals = get_expected_load_totals(storage_client, {area})
+    loaded_paths = find_files_in_load_history(bq_project, dataset, expected_load_totals)
 
-    logging.info("Inspecting load history in bigquery...")
-    load_history = get_load_history(bq_project, dataset, start_date)
-    tdr_load_totals = {
-        load_history_row[0]: load_history_row[1]
-        for load_history_row in load_history
-    }
-
-    for unexpected_area in tdr_load_totals.keys() - expected_load_totals.keys():
-        logging.warning(f"⚠️ {unexpected_area} not in manifest but was imported")
-
-    success = True
-    total_loaded_count = 0
-    total_expected_count = 0
-    for area, expected_count in expected_load_totals.items():
-        total_expected_count += expected_count
-        if area in tdr_load_totals:
-            total_loaded_count += tdr_load_totals[area]
-            expected_count = expected_load_totals[area]
-            if expected_count != tdr_load_totals[area]:
-                logging.error(f"❌ Mismatched file count: staging_area = {area}, "
-                              f"imported = {tdr_load_totals[area]}, expected = {expected_count}")
-                success = False
-            else:
-                logging.info(
-                    f"✅ staging_area = {area}, imported = {tdr_load_totals[area]}, expected = {expected_count}")
+    for a, target_paths in expected_load_totals.items():
+        loaded_paths = loaded_paths[a]
+        r = target_paths - loaded_paths
+        if len(r) > 0:
+            logging.warning(
+                f"❌ area = {a} Mismatched loaded paths; expected to find {len(target_paths)}, found {len(loaded_paths)}")
+            logging.warning(r)
         else:
-            logging.error(f"❌ {area} has not been imported")
-            success = False
-    logging.info('-' * 80)
-    logging.info(
-        f"Total staging areas = {len(expected_load_totals.keys())}, total files staged = {total_expected_count}, total files loaded = {total_loaded_count}")
-    return success
+            logging.info(f"✅  area = {a}, expected = {len(target_paths)}, found {len(loaded_paths)}, diff = {r}")
+
+
+def verify(manifest_file: str, gs_project: str, bq_project: str, dataset: str, pool_size: int) -> bool:
+    logging.info("Parsing manifest...")
+    staging_areas = parse_manifest_file(manifest_file)
+    logging.info(f"{len(staging_areas)} staging areas in manifest.")
+    logging.info(f"Inspecting staging areas (pool_size = {pool_size})...")
+
+    with Pool(pool_size) as p:
+        curried = partial(process_staging_area, gs_project=gs_project, bq_project=bq_project, dataset=dataset)
+        p.map(curried, staging_areas)
+
+    return True
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--start-date", required=True)
     parser.add_argument("-f", "--manifest-file", required=True)
     parser.add_argument("-g", "--gs-project", required=True)
     parser.add_argument("-b", "--bq-project", required=True)
     parser.add_argument("-d", "--dataset", required=True)
+    parser.add_argument("-p", "--pool-size", type=int, default=4)
     args = parser.parse_args()
 
-    result = verify(args.start_date, args.manifest_file, args.gs_project, args.bq_project, args.dataset)
+    result = verify(args.manifest_file, args.gs_project, args.bq_project, args.dataset, args.pool_size)
     if not result:
         sys.exit(1)
