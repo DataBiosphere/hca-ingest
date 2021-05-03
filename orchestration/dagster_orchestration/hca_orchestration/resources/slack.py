b1ea8d050394b22c40aa7590c06f35e65fa50c53
Steve Pletcher <spletche@broadinstitute.org>
2021-05-03 15:16:31 -0400

    Fix Dagit config resource ref in snapshot slack hooks

--- orchestration/dagster_orchestration/hca_orchestration/resources/slack.py
+++ orchestration/dagster_orchestration/hca_orchestration/resources/slack.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
 
 import slack
-from dagster import configured, DagsterLogManager, resource, String, StringSource
+from dagster import DagsterLogManager, resource, String, StringSource
 from dagster.core.execution.context.init import InitResourceContext
-
-from hca_orchestration.support.typing import DagsterConfigDict
 
 
 @dataclass
@@ -33,20 +31,8 @@ class LiveSlackClient:
     'channel': String,
     'token': StringSource,
 })
-def untokened_live_slack_client(init_context: InitResourceContext) -> LiveSlackClient:
+def live_slack_client(init_context: InitResourceContext) -> LiveSlackClient:
     return LiveSlackClient(
         slack.WebClient(init_context.resource_config['token']),
         init_context.resource_config['channel'],
     )
-
-
-# this can't be fully configured using preconfigure_for_mode, since SLACK_TOKEN is a secret
-@configured(untokened_live_slack_client, {'channel': String})
-def live_slack_client(config: DagsterConfigDict) -> DagsterConfigDict:
-    return {
-        'token': {'env': 'SLACK_TOKEN'},
-        **config,
-    }
-
-
-live_slack_client.__name__ = 'live_slack_client'
