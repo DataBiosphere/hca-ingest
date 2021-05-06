b304eb9d5df186f19213ea2e7cbb29c1260d126c
Drew Herbst <aherbst@broadinstitute.org>
2021-05-06 11:39:20 -0400

    Autospec the bigquery client

--- orchestration/dagster_orchestration/hca_orchestration/resources/bigquery.py
+++ orchestration/dagster_orchestration/hca_orchestration/resources/bigquery.py
@@ -10,14 +10,6 @@ def bigquery_client(init_context: InitResourceContext) -> Client:
     return Client(credentials=get_credentials())
 
 
-# class NoopBigQueryClient:
-#     def create_dataset(self, dataset: Dataset) -> None:
-#         pass
-#
-#     def query(self, query: str, job_config: QueryJobConfig, location: str):
-#         pass
-
-
 @resource
 def noop_bigquery_client(init_context: InitResourceContext) -> Client:
-    return create_autospec(Client)  # Mock(spec=Client) #NoopBigQueryClient()
+    return create_autospec(Client)
