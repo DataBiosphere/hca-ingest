54813bbb5c801aa01baa2bad4c3db2685d941e60
Steve Pletcher <spletche@broadinstitute.org>
2021-05-10 16:46:07 -0400

    properly reference dagster type for str subclasses

--- orchestration/dagster_orchestration/hca_orchestration/support/typing.py
+++ orchestration/dagster_orchestration/hca_orchestration/support/typing.py
@@ -4,10 +4,12 @@ base can live here, for easy reference and descriptive naming.
 """
 
 from dagster import make_python_type_usable_as_dagster_type
+from dagster.core.types.dagster_type import String as DagsterString
+
 
 
 class HcaScratchDatasetName(str):
     pass
 
 
-make_python_type_usable_as_dagster_type(HcaStagingDatasetName, str)
+make_python_type_usable_as_dagster_type(HcaStagingDatasetName, DagsterString)
