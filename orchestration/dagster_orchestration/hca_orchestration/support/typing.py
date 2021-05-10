eda685fcec3939ab4c4c64939d153ad2151ec063
Steve Pletcher <spletche@broadinstitute.org>
2021-05-10 16:03:47 -0400

    also include HcaStagingDatasetName

--- orchestration/dagster_orchestration/hca_orchestration/support/typing.py
+++ orchestration/dagster_orchestration/hca_orchestration/support/typing.py
@@ -3,6 +3,11 @@ Complex type signatures that appear multiple times throughout the code
 base can live here, for easy reference and descriptive naming.
 """
 
+from dagster import make_python_type_usable_as_dagster_type
+
 
 class HcaScratchDatasetName(str):
     pass
+
+
+make_python_type_usable_as_dagster_type(HcaStagingDatasetName, str)
