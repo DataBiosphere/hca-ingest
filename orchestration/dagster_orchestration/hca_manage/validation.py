700323684d9eb68ec35a0962a1cee8ea5d3105fe
Eric Park <77357127+eparkmonster@users.noreply.github.com>
2021-05-21 15:29:42 -0400

    Pre-flight validation check wrangler CLI script (#125)
    * Create validation.py
    
    * Add return types
    
    * add ignore import missing jsonschema
    
    * add main guard
    
    * update validation
    
    * add return types
    
    * return type
    
    * fetch schema
    
    * update Schema fetcher return type
    
    * check if file is json
    
    * updated json string
    
    * change return types
    
    * add typing
    
    * rearranged libraries

---
+++ orchestration/dagster_orchestration/hca_manage/validation.py
@@ -0,0 +1,96 @@
+from dataclasses import dataclass
+import json
+from jsonschema import validate
+import logging
+import requests
+from typing import Optional, Dict, Any
+
+from google.cloud import storage
+from urllib.parse import urlparse
+
+from hca_manage.common import DefaultHelpParser
+
+
+class SchemaFetcher:
+    def __init__(self) -> None:
+        self._schema_cache: dict[str, dict[str, str]] = {}
+
+    def fetch_schema(self, path: str) -> Any:
+        if path in self._schema_cache:
+            return self._schema_cache[path]
+
+        raw_schema = requests.get(path).json()
+        self._schema_cache[path] = raw_schema
+        return raw_schema
+
+
+@dataclass
+class GsBucketWithPrefix:
+    bucket: str
+    prefix: str
+
+
+def parse_gs_path(raw_gs_path: str) -> GsBucketWithPrefix:
+    url_result = urlparse(raw_gs_path)
+    return GsBucketWithPrefix(url_result.netloc, url_result.path[1:])
+
+
+def validate_json(blob: storage.Blob, schema_fetcher: SchemaFetcher) -> Optional[Exception]:
+    """
+    Validate that the JSON file blob follows the schema in the describedBy
+    :param blob: JSON file blob to check
+    :param schema_fetcher: Schema Fetcher that contains a set of unique schemas
+    :return: An exception if JSON is invalid, return None if valid
+    """
+    try:
+        data = json.loads(blob.download_as_string())
+        schema_path = data["describedBy"]
+        schema = schema_fetcher.fetch_schema(schema_path)
+        validate(instance=data, schema=schema)
+        return None
+    except Exception as e:
+        return e
+
+
+def validate_directory(path: str, bucket: storage.Client.bucket, schema_fetcher: SchemaFetcher) -> None:
+    """
+    Validate that the directory in the Google storage bucket is valid
+    :param path: Google stage path name
+    :param bucket: Google storage bucket name
+    """
+    valid_files_in_dir = []
+    invalid_files_in_dir = {}
+    for blob in bucket.list_blobs(prefix=path):
+        if blob.name.endswith('json'):
+            json_error = validate_json(blob, schema_fetcher)
+            if json_error is None:
+                valid_files_in_dir.append(blob.name)
+            else:
+                invalid_files_in_dir[blob.name] = json_error
+        else:
+            valid_files_in_dir.append(blob.name)
+    if len(valid_files_in_dir) == 0 and len(invalid_files_in_dir) == 0:
+        logging.error(f"{path} File Path doesn't exist")
+    elif len(invalid_files_in_dir) > 0:
+        logging.error(f"List of invalid json files {invalid_files_in_dir}")
+    else:
+        logging.info('File path and Json are valid')
+
+
+def run(arguments: Optional[list[str]] = None) -> None:
+    parser = DefaultHelpParser(description="CLI to manage validate GS path and json files.")
+    parser.add_argument("-p", "--path", help="GS path to validate", required=True)
+    args = parser.parse_args(arguments)
+
+    storage_client = storage.Client()
+    gs_bucket = parse_gs_path(args.path)
+    bucket = storage_client.bucket(gs_bucket.bucket)
+    schema_fetcher = SchemaFetcher()
+
+    well_known_dirs = {'/data', '/descriptors', '/links', '/metadata'}
+    for dir in well_known_dirs:
+        validate_directory(gs_bucket.prefix + dir, bucket, schema_fetcher)
+
+
+if __name__ == "__main__":
+    run()
