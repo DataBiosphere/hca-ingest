c1631ff06b538c246b1faa3e940c3ef2b63e6937
Drew Herbst <aherbst@broadinstitute.org>
2021-06-02 13:31:36 -0400

    DRY

--- orchestration/dagster_orchestration/hca_orchestration/tests/e2e/test_load_hca.py
+++ orchestration/dagster_orchestration/hca_orchestration/tests/e2e/test_load_hca.py
@@ -13,89 +13,39 @@ def test_load_hca(load_hca_run_config, dataset_name, tdr_bigquery_client):
         run_config=load_hca_run_config
     )
 
-    sequence_file_rows = _query_metadata_table(
-        "sequence_file",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(sequence_file_rows) > 0, "Should have sequence_file rows"
+    assert_metadata_loaded("sequence_file", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("cell_suspension", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("collection_protocol", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("dissociation_protocol", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("donor_organism", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("library_preparation_protocol", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("process", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("project", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("sequencing_protocol", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("specimen_from_organism", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("links", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("specimen_from_organism", dataset_name, tdr_bigquery_client)
+    assert_metadata_loaded("specimen_from_organism", dataset_name, tdr_bigquery_client)
+    assert_data_loaded("sequence_file", dataset_name, tdr_bigquery_client)
 
-    cell_suspension_rows = _query_metadata_table(
-        "cell_suspension",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(cell_suspension_rows) > 0, "Should have cell_suspension rows"
 
-    collection_protocol_rows = _query_metadata_table(
-        "collection_protocol",
+def assert_data_loaded(file_type: str, dataset_name: str, tdr_bigquery_client: Client):
+    files_loaded = _query_files_loaded(
+        file_type,
         dataset_name,
         tdr_bigquery_client
     )
-    assert len(collection_protocol_rows) > 0, "Should have collection_protocol rows"
+    assert len(files_loaded) > 0, f"Should have loaded {file_type} data files"
 
-    dissociation_protocol_rows = _query_metadata_table(
-        "dissociation_protocol",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(dissociation_protocol_rows) > 0, "Should have dissociation_protocol rows"
 
-    donor_organism_rows = _query_metadata_table(
-        "donor_organism",
+def assert_metadata_loaded(metadata_type: str, dataset_name: str, tdr_bigquery_client: Client):
+    data_loaded = _query_metadata_table(
+        metadata_type,
         dataset_name,
         tdr_bigquery_client
     )
-    assert len(donor_organism_rows) > 0, "Should have donor_organism rows"
 
-    library_preparation_protocol_rows = _query_metadata_table(
-        "library_preparation_protocol",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(library_preparation_protocol_rows) > 0, "Should have library_preparation_protocol rows"
-
-    process_rows = _query_metadata_table(
-        "process",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(process_rows) > 0, "Should have process rows"
-
-    project_rows = _query_metadata_table(
-        "process",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(project_rows) > 0, "Should have project rows"
-
-    sequencing_protocol_rows = _query_metadata_table(
-        "sequencing_protocol",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(sequencing_protocol_rows) > 0, "Should have sequencing_protocol rows"
-
-    specimen_from_organism_rows = _query_metadata_table(
-        "specimen_from_organism",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(specimen_from_organism_rows) > 0, "Should have specimen_from_organism rows"
-
-    links_rows = _query_metadata_table(
-        "links",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(links_rows) > 0, "Should have links rows"
-
-    sequence_files_loaded = _query_files_loaded(
-        "sequence_file",
-        dataset_name,
-        tdr_bigquery_client
-    )
-    assert len(sequence_files_loaded) > 0, "Should have loaded sequence_file data files"
+    assert len(data_loaded) > 0, f"Should have loaded {metadata_type} rows"
 
 
 def _query_metadata_table(metadata_type: str, dataset_name: str, client: Client):
