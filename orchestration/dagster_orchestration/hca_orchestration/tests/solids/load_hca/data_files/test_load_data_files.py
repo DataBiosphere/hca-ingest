6e036f3cfe3146211da3c8dfe350b96641659735
Raaid Arshad <rarshad@broadinstitute.org>
2021-06-08 08:25:39 -0400

    refactor test_load_data_files.py to the pytest way instead of the unittest way

--- orchestration/dagster_orchestration/hca_orchestration/tests/solids/load_hca/data_files/test_load_data_files.py
+++ orchestration/dagster_orchestration/hca_orchestration/tests/solids/load_hca/data_files/test_load_data_files.py
@@ -1,4 +1,3 @@
-import unittest
 from unittest.mock import Mock
 
 from dagster import execute_solid, ModeDefinition, SolidExecutionResult, ResourceDefinition
@@ -13,62 +12,62 @@ from hca_orchestration.solids.load_hca.data_files.load_data_files import diff_fi
 from hca_orchestration.support.typing import HcaScratchDatasetName
 
 
-class LoadDataFilesTestCase(unittest.TestCase):
-    def setUp(self):
-        self.load_datafiles_test_mode: ModeDefinition = ModeDefinition(
-            name="test",
-            resource_defs={
-                "gcs": mock_storage_client,
-                "bigquery_client": noop_bigquery_client,
-                "bigquery_service": ResourceDefinition.mock_resource(),
-                "target_hca_dataset": ResourceDefinition.hardcoded_resource(
-                    TargetHcaDataset(
-                        "fake_dataset_name",
-                        "1234abc",
-                        "fake_hca_project_id",
-                        "fake_billing_profile_id"
-                    )),
-                "scratch_config": ResourceDefinition.hardcoded_resource(
-                    ScratchConfig(
-                        "fake_bucket",
-                        "fake_prefix",
-                        "fake_bq_project",
-                        "fake_dataset_prefix",
-                        1
-                    )
-                ),
-                "load_tag": ResourceDefinition.hardcoded_resource("fake_load_tag")
-            }
-        )
+load_datafiles_test_mode: ModeDefinition = ModeDefinition(
+    name="test",
+    resource_defs={
+        "gcs": mock_storage_client,
+        "bigquery_client": noop_bigquery_client,
+        "bigquery_service": ResourceDefinition.mock_resource(),
+        "target_hca_dataset": ResourceDefinition.hardcoded_resource(
+            TargetHcaDataset(
+                "fake_dataset_name",
+                "1234abc",
+                "fake_hca_project_id",
+                "fake_billing_profile_id"
+            )),
+        "scratch_config": ResourceDefinition.hardcoded_resource(
+            ScratchConfig(
+                "fake_bucket",
+                "fake_prefix",
+                "fake_bq_project",
+                "fake_dataset_prefix",
+                1
+            )
+        ),
+        "load_tag": ResourceDefinition.hardcoded_resource("fake_load_tag")
+    }
+)
 
-    def test_diff_file_loads(self):
-        result: SolidExecutionResult = execute_solid(
-            diff_file_loads,
-            mode_def=self.load_datafiles_test_mode,
-            input_values={
-                'scratch_dataset_name': HcaScratchDatasetName("fake_bq_project.testing_dataset_prefix_fake_load_tag")
-            }
-        )
 
-        self.assertTrue(result.success)
-        self.assertEqual(len(result.output_values["control_file_path"]), 10)
+def test_diff_file_loads():
+    result: SolidExecutionResult = execute_solid(
+        diff_file_loads,
+        mode_def=load_datafiles_test_mode,
+        input_values={
+            'scratch_dataset_name': HcaScratchDatasetName("fake_bq_project.testing_dataset_prefix_fake_load_tag")
+        }
+    )
 
-    def test_run_bulk_file_ingest_should_return_a_jade_job_id(self):
-        job_id = "fake_job_id"
-        data_repo = Mock(spec=RepositoryApi)
-        job_response = Mock(spec=JobModel)
-        job_response.id = job_id
-        data_repo.bulk_file_load = Mock(return_value=job_response)
-        self.load_datafiles_test_mode.resource_defs["data_repo_client"] = ResourceDefinition.hardcoded_resource(
-            data_repo)
+    assert result.success
+    assert len(result.output_values["control_file_path"]) == 10
 
-        result: SolidExecutionResult = execute_solid(
-            run_bulk_file_ingest,
-            mode_def=self.load_datafiles_test_mode,
-            input_values={
-                'control_file_path': HcaScratchDatasetName("gs://fake/example/123.txt")
-            }
-        )
 
-        self.assertTrue(result.success)
-        self.assertEqual(result.output_values["result"], job_id, f"Job ID should be {job_id}")
+def test_run_bulk_file_ingest_should_return_a_jade_job_id():
+    job_id = "fake_job_id"
+    data_repo = Mock(spec=RepositoryApi)
+    job_response = Mock(spec=JobModel)
+    job_response.id = job_id
+    data_repo.bulk_file_load = Mock(return_value=job_response)
+    load_datafiles_test_mode.resource_defs["data_repo_client"] = ResourceDefinition.hardcoded_resource(
+        data_repo)
+
+    result: SolidExecutionResult = execute_solid(
+        run_bulk_file_ingest,
+        mode_def=load_datafiles_test_mode,
+        input_values={
+            'control_file_path': HcaScratchDatasetName("gs://fake/example/123.txt")
+        }
+    )
+
+    assert result.success
+    assert result.output_values["result"] == job_id, f"Job ID should be {job_id}"
