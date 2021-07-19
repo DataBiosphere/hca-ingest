64f6e8b9ce63f42435e0c24cafacedb785699b99
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-07-19 09:38:29 -0400

    [DSPDC-1860] Add snapshot retrieval and snapshot policy update operations (#160)
    Why
    
    We need the ability to add policy members to a snapshot.
    
    This PR
    
    Adds CLI options to our snapshot tool that enable querying and adding snapshot policy members
    Customizes the snapshot policy members based on a new managed_access bool and threads that through to the relevant pipeline code

--- orchestration/dagster_orchestration/hca_manage/snapshot.py
+++ orchestration/dagster_orchestration/hca_manage/snapshot.py
@@ -6,7 +6,8 @@ from re import search
 import sys
 from typing import Optional
 
-from data_repo_client import RepositoryApi, SnapshotRequestModel, SnapshotRequestContentsModel, EnumerateSnapshotModel
+from data_repo_client import RepositoryApi, SnapshotRequestModel, SnapshotRequestContentsModel, \
+    EnumerateSnapshotModel, PolicyMemberRequest, PolicyResponse
 from dagster_utils.contrib.data_repo.jobs import poll_job, JobPollException
 from dagster_utils.contrib.data_repo.typing import JobId
 
@@ -51,8 +52,53 @@ def run(arguments: Optional[list[str]] = None) -> None:
     snapshot_query.add_argument("-n", "--snapshot_name", help="Name of snapshot to filter for")
     snapshot_query.set_defaults(func=_query_snapshot)
 
+    snapshot_add_policy_member = subparsers.add_parser("add_policy_member")
+    snapshot_add_policy_member.add_argument(
+        "-p",
+        "--policy_member",
+        help="Email address of user to add as a policy member",
+        required=True)
+    snapshot_add_policy_member.add_argument("-i", "--snapshot_id", help="Id of snapshot", required=True)
+    snapshot_add_policy_member.add_argument(
+        "-n",
+        "--policy_name",
+        help="Name of policy (one of steward, reader, discoverer)",
+        required=True)
+    snapshot_add_policy_member.set_defaults(func=_add_policy_member)
+
+    snapshot_retrieve_policies = subparsers.add_parser("retrieve_snapshot_policies")
+    snapshot_retrieve_policies.add_argument("-i", "--snapshot_id", help="Id of snapshot", required=True)
+    snapshot_retrieve_policies.set_defaults(func=_retrieve_policies)
+
     args = parser.parse_args(arguments)
     args.func(args)
+
+
+@tdr_operation
+def _retrieve_policies(args: argparse.Namespace) -> None:
+    host = data_repo_host[args.env]
+
+    snapshot_id = args.snapshot_id
+    hca = SnapshotManager(environment=args.env, data_repo_client=get_api_client(host=host))
+    response = hca.retrieve_policies(snapshot_id=snapshot_id)
+    logging.info(response)
+
+
+@tdr_operation
+def _add_policy_member(args: argparse.Namespace) -> None:
+    host = data_repo_host[args.env]
+
+    policy_member = args.policy_member
+    snapshot_id = args.snapshot_id
+    policy_name = args.policy_name
+
+    if not query_yes_no(
+            f"This will add {policy_member} as a {policy_name} to snapshot id = {snapshot_id}, are you sure?"):
+        return
+
+    hca = SnapshotManager(environment=args.env, data_repo_client=get_api_client(host=host))
+    response = hca.add_policy_member(policy_member, policy_name, snapshot_id)
+    logging.info(response)
 
 
 @tdr_operation
@@ -97,8 +143,21 @@ class SnapshotManager:
     reader_list: list[str] = field(init=False)
 
     def __post_init__(self) -> None:
-        self.reader_list = {
-            "dev": ["hca-snapshot-readers@dev.test.firecloud.org"],
+        self.managed_access_reader_list = {
+            "dev": [
+                "hca-snapshot-readers@dev.test.firecloud.org",
+                "monster-dev@dev.test.firecloud.org",
+                "azul-dev@dev.test.firecloud.org"
+            ],
+            "prod": ["hca-snapshot-readers@firecloud.org", "monster@firecloud.org"]
+        }[self.environment]
+        self.publc_access_reader_list = {
+            "dev": [
+                "hca-snapshot-readers@dev.test.firecloud.org",
+                "monster-dev@dev.test.firecloud.org",
+                "azul-dev@dev.test.firecloud.org",
+                "azul-public-dev@dev.test.firecloud.org"
+            ],
             "prod": ["hca-snapshot-readers@firecloud.org", "monster@firecloud.org"]
         }[self.environment]
 
@@ -110,21 +169,23 @@ class SnapshotManager:
         snapshot_date = snapshot_date or datetime.today().date()
         return self.submit_snapshot_request_with_name(self.snapshot_name(qualifier, snapshot_date))
 
-    def submit_snapshot_request_with_name(self, snapshot_name: str) -> JobId:
+    def submit_snapshot_request_with_name(self, snapshot_name: str, managed_access: bool = False) -> JobId:
         """
         Submit a snapshot creation request.
-        :param snapshot_name: name of snapshot to create
+        :param snapshot_name: name of snapshot to created
+        :param managed_access: Determine which set of readers to grant access to this snapshot (default = False)
         :return: Job ID of the snapshot creation job
         """
         if not search(SNAPSHOT_NAME_REGEX, snapshot_name):
             raise InvalidSnapshotNameException(f"Snapshot name {snapshot_name} is invalid")
 
+        reader_list = self.managed_access_reader_list if managed_access else self.publc_access_reader_list
         snapshot_request = SnapshotRequestModel(
             name=snapshot_name,
             profile_id=self.data_repo_profile_id,
             description=f"Create snapshot {snapshot_name}",
             contents=[SnapshotRequestContentsModel(dataset_name=self.dataset, mode="byFullView")],
-            readers=self.reader_list
+            readers=reader_list
         )
 
         logging.info(snapshot_request)
@@ -183,7 +244,14 @@ class SnapshotManager:
         return job_id
 
     def query_snapshot(self, snapshot_name: Optional[str] = None) -> EnumerateSnapshotModel:
-        return self.data_repo_client.enumerate_snapshots(filter=snapshot_name)
+        return self.data_repo_client.enumerate_snapshots(filter=snapshot_name, limit=1000)
+
+    def add_policy_member(self, policy_member: str, policy_name: str, snapshot_id: str) -> PolicyResponse:
+        payload = PolicyMemberRequest(email=policy_member)
+        return self.data_repo_client.add_snapshot_policy_member(snapshot_id, policy_name, policy_member=payload)
+
+    def retrieve_policies(self, snapshot_id: str) -> PolicyResponse:
+        return self.data_repo_client.retrieve_snapshot_policies(id=snapshot_id)
 
 
 if __name__ == '__main__':
