98230b27592cb4ae9731b80003bcc5bcc0849d79
Drew Herbst <aherbst@broadinstitute.org>
2021-08-02 15:53:41 -0400

    Testing

--- orchestration/dagster_orchestration/hca_orchestration/tests/e2e/test_load_hca.py
+++ orchestration/dagster_orchestration/hca_orchestration/tests/e2e/test_load_hca.py
@@ -7,28 +7,27 @@ from hca_orchestration.pipelines import load_hca
 
 @pytest.mark.e2e
 def test_load_hca(load_hca_run_config, dataset_name, tdr_bigquery_client, dataset_info):
-    assert True
-    # execute_pipeline(
-    #     load_hca,
-    #     mode="local",
-    #     run_config=load_hca_run_config
-    # )
-    #
-    # bq_project = dataset_info.dataset_data_project_id
-    # assert_metadata_loaded("sequence_file", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("cell_suspension", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("collection_protocol", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("dissociation_protocol", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("donor_organism", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("library_preparation_protocol", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("process", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("project", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("sequencing_protocol", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("specimen_from_organism", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("links", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("specimen_from_organism", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_metadata_loaded("specimen_from_organism", dataset_name, bq_project, tdr_bigquery_client)
-    # assert_data_loaded("sequence_file", dataset_name, bq_project, tdr_bigquery_client)
+    execute_pipeline(
+        load_hca,
+        mode="local",
+        run_config=load_hca_run_config
+    )
+
+    bq_project = dataset_info.dataset_data_project_id
+    assert_metadata_loaded("sequence_file", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("cell_suspension", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("collection_protocol", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("dissociation_protocol", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("donor_organism", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("library_preparation_protocol", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("process", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("project", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("sequencing_protocol", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("specimen_from_organism", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("links", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("specimen_from_organism", dataset_name, bq_project, tdr_bigquery_client)
+    assert_metadata_loaded("specimen_from_organism", dataset_name, bq_project, tdr_bigquery_client)
+    assert_data_loaded("sequence_file", dataset_name, bq_project, tdr_bigquery_client)
 
 
 def assert_data_loaded(file_type: str, dataset_name: str, bq_project: str, tdr_bigquery_client: Client):
