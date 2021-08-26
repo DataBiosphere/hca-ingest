0f58f50c106ad67f9536b6c8151ba4abe6acad5a
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-08-26 10:46:59 -0400

    [DSPDC-1814] Update desired dev refresh partitions (#188)
    Why
    
    We have an updated list of projects for the dev refresh.
    
    This PR
    
    Updates the csv of projects to copy

---
+++ orchestration/dagster_orchestration/hca_orchestration/solids/validate_dataset.py
@@ -0,0 +1,22 @@
+from dagster import op, Failure, In, Nothing
+from dagster.core.execution.context.compute import AbstractComputeExecutionContext
+
+from hca_manage.check import CheckManager
+from hca_orchestration.models.hca_dataset import TdrDataset
+
+
+@op(
+    required_resource_keys={"data_repo_client", "target_hca_dataset"},
+    ins={"start": In(Nothing)}
+)
+def validate_copied_dataset(context: AbstractComputeExecutionContext) -> None:
+    target_hca_dataset: TdrDataset = context.resources.target_hca_dataset
+    result = CheckManager(
+        environment="dev",
+        project=target_hca_dataset.project_id,
+        dataset=target_hca_dataset.dataset_name,
+        data_repo_client=context.resources.data_repo_client
+    ).check_for_all()
+
+    if result.has_problems():
+        raise Failure(f"Dataset {target_hca_dataset.dataset_name} failed validation")
--- orchestration/dagster_orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
+++ orchestration/dagster_orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
@@ -1,9 +1,15 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, Mock
 
 from hca_orchestration.pipelines.copy_project import copy_project
 
 
-def test_copy_project() -> None:
+@patch("hca_manage.bq_managers.DanglingFileRefManager.get_rows", return_value=set())
+@patch("hca_manage.bq_managers.NullFileRefManager.get_rows", return_value=set())
+@patch("hca_manage.bq_managers.NullFileRefManager.get_file_table_names", return_value=set())
+@patch("hca_manage.bq_managers.DuplicatesManager.get_rows", return_value=set())
+@patch("hca_manage.bq_managers.DuplicatesManager.get_all_table_names", return_value=set())
+def test_copy_project(mock_all_table_names: Mock, mock_duplicates: Mock, mock_file_table_names: Mock,
+                      mock_null_filerefs: Mock, mock_dangling_proj_refs: Mock) -> None:
     result = copy_project.execute_in_process(
         resources={
             "bigquery_client": MagicMock(),
