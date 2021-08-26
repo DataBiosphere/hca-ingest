f3cf7d43c5febeedd51683980118b1e2dc3d7226
Drew Herbst <aherbst@broadinstitute.org>
2021-08-26 10:17:13 -0400

    Validate dataset on copy finish

--- orchestration/dagster_orchestration/hca_orchestration/pipelines/copy_project.py
+++ orchestration/dagster_orchestration/hca_orchestration/pipelines/copy_project.py
@@ -6,16 +6,19 @@ from hca_orchestration.solids.copy_project.inject_file_ids import inject_file_id
 from hca_orchestration.solids.copy_project.subgraph_hydration import hydrate_subgraphs
 from hca_orchestration.solids.copy_project.tabular_data_ingestion import ingest_tabular_data
 from hca_orchestration.solids.load_hca.stage_data import clear_scratch_dir
+from hca_orchestration.solids.validate_dataset import validate_copied_dataset
 
 
 @graph
 def copy_project() -> None:
-    delete_outdated_tabular_data(
-        inject_file_ids(
-            ingest_tabular_data(
-                ingest_data_files(
-                    hydrate_subgraphs(
-                        clear_scratch_dir()
+    validate_copied_dataset(
+        delete_outdated_tabular_data(
+            inject_file_ids(
+                ingest_tabular_data(
+                    ingest_data_files(
+                        hydrate_subgraphs(
+                            clear_scratch_dir()
+                        )
                     )
                 )
             )
