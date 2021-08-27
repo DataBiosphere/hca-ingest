c6a5a61d0a7f112f341c890f3ae4601c6c134b82
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2021-08-27 11:37:14 -0400

    [DSPDC-1814] Materialize asset when copying projects (#194)
    Why
    
    We should materialize an asset for tracking purposes when copying projects.
    
    This PR
    
    Materializes the asset after validation is complete.

--- orchestration/dagster_orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
+++ orchestration/dagster_orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
@@ -1,6 +1,8 @@
 from unittest.mock import MagicMock, patch, Mock
 
 from hca_orchestration.pipelines.copy_project import copy_project
+from hca_orchestration.models.hca_dataset import TdrDataset
+from hca_orchestration.resources.hca_project_config import HcaProjectCopyingConfig
 
 
 @patch("hca_manage.bq_managers.DanglingFileRefManager.get_rows", return_value=set())
@@ -17,8 +19,8 @@ def test_copy_project(mock_all_table_names: Mock, mock_duplicates: Mock, mock_fi
             "gcs": MagicMock(),
             "scratch_config": MagicMock(),
             "bigquery_service": MagicMock(),
-            "hca_project_copying_config": MagicMock(),
-            "target_hca_dataset": MagicMock(),
+            "hca_project_copying_config": HcaProjectCopyingConfig("fake_source_project_id", "fake_source_snapshot_name", "fake_bq_project_id"),
+            "target_hca_dataset": TdrDataset("fake_name", "fake_id", "fake_gcp_project_id", "fake_billing_profile_id"),
             "load_tag": MagicMock(),
         })
     assert result.success
