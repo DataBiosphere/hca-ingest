d3b2c5ec01d8aec00166a78405751387d35e2934
Drew Herbst <aherbst@broadinstitute.org>
2021-08-27 11:27:52 -0400

    Fix test

--- orchestration/dagster_orchestration/hca_orchestration/solids/validate_dataset.py
+++ orchestration/dagster_orchestration/hca_orchestration/solids/validate_dataset.py
@@ -1,16 +1,24 @@
-from dagster import op, Failure, In, Nothing
+from dagster import op, Failure, In, Nothing, AssetMaterialization, AssetKey, Out, Output
 from dagster.core.execution.context.compute import AbstractComputeExecutionContext
 
+from hca_manage.common import ProblemCount
 from hca_manage.check import CheckManager
 from hca_orchestration.models.hca_dataset import TdrDataset
+from hca_orchestration.resources.hca_project_config import HcaProjectCopyingConfig
 
 
 @op(
-    required_resource_keys={"data_repo_client", "target_hca_dataset"},
+    required_resource_keys={
+        "data_repo_client",
+        "target_hca_dataset",
+        "hca_project_copying_config",
+    },
     ins={"start": In(Nothing)}
 )
-def validate_copied_dataset(context: AbstractComputeExecutionContext) -> None:
+def validate_copied_dataset(context: AbstractComputeExecutionContext) -> ProblemCount:
     target_hca_dataset: TdrDataset = context.resources.target_hca_dataset
+    hca_project_config: HcaProjectCopyingConfig = context.resources.hca_project_copying_config
+
     result = CheckManager(
         environment="dev",
         project=target_hca_dataset.project_id,
@@ -20,3 +28,11 @@ def validate_copied_dataset(context: AbstractComputeExecutionContext) -> None:
 
     if result.has_problems():
         raise Failure(f"Dataset {target_hca_dataset.dataset_name} failed validation")
+
+    yield AssetMaterialization(
+        asset_key=AssetKey([hca_project_config.source_hca_project_id, target_hca_dataset.project_id,
+                            target_hca_dataset.dataset_name, target_hca_dataset.dataset_id]),
+        partition=f"{hca_project_config.source_hca_project_id}"
+    )
+
+    yield Output(result)
