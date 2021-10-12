c6d907cc44408535d2391ff035a153477fbada93
Quazi <59845076+quazi-broad@users.noreply.github.com>
2021-10-12 10:05:58 -0400

    Update HCA Slack Msg Format (#236)
    * Switching from kv blocks to text input to slack tooling
    * Refactored logic around passing text to the slack client to be more idiomatic.
    * Applied changes to all slack notifications using the key_value_slack_blocks approach.

--- orchestration/hca_orchestration/pipelines/validate_ingress.py
+++ orchestration/hca_orchestration/pipelines/validate_ingress.py
@@ -1,7 +1,6 @@
 from dagster import failure_hook, HookContext, graph, resource, InitResourceContext
 
 from hca_manage.validation import HcaValidator
-from hca_orchestration.contrib.slack import key_value_slack_blocks
 from hca_orchestration.solids.validate_ingress import pre_flight_validate, notify_slack_of_succesful_ingress_validation
 
 
@@ -14,10 +13,11 @@ def staging_area_validator(init_context: InitResourceContext) -> HcaValidator:
     required_resource_keys={'slack', 'dagit_config'}
 )
 def validation_failed_notification(context: HookContext) -> None:
-    kvs = {
-        "Dagit link": f'<{context.resources.dagit_config.run_url(context.run_id)}|View in Dagit>'
-    }
-    context.resources.slack.send_message(blocks=key_value_slack_blocks("Validation Ingress Failed", key_values=kvs))
+    lines = ("Validation Ingress Failed",
+             "Dagit link: " + f"<{context.resources.dagit_config.run_url(context.run_id)}|View in Dagit>"
+             )
+    slack_msg_text = "\n".join(lines)
+    context.resources.slack.send_message(text=slack_msg_text)
 
 
 @graph
