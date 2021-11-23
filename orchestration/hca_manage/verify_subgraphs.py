0767f67f9b01c36ea01c33282b489f91aa144303
Drew Herbst <aherbst@broadinstitute.org>
2021-11-23 08:58:44 -0500

    Add subgraph verification step

--- orchestration/hca_manage/verify_subgraphs.py
+++ orchestration/hca_manage/verify_subgraphs.py
@@ -4,14 +4,18 @@ Verifies that all nodes in the subgraphs of a dataset or snapshot are loaded
 import argparse
 from typing import Optional
 
-from google.cloud.bigquery import Client, ArrayQueryParameter, QueryJobConfig, Row
+from google.cloud.bigquery import Client, ArrayQueryParameter
+from google.cloud.bigquery.table import RowIterator
 
+from hca_orchestration.contrib.bigquery import BigQueryService
+from hca_manage.common import data_repo_host, get_api_client
+from hca_orchestration.contrib.data_repo.data_repo_service import DataRepoService
 from hca_orchestration.models.entities import MetadataEntity, build_subgraph_from_links_row
 from hca_orchestration.support.subgraphs import build_subgraph_nodes
 from hca_orchestration.support.typing import MetadataType
 
 
-def verify_all_subgraphs_in_dataset(links_rows: list[Row], bq_project: str, dataset: str, client: Client) -> None:
+def verify_all_subgraphs_in_dataset(links_rows: RowIterator, bq_project: str, dataset: str, client: Client) -> None:
     links = [
         build_subgraph_from_links_row(row) for row in links_rows
     ]
@@ -23,7 +27,7 @@ def verify_all_subgraphs_in_dataset(links_rows: list[Row], bq_project: str, data
 
 
 def verify_entities_loaded(entity_type: MetadataType, expected_entities: list[MetadataEntity], bq_project: str,
-                           dataset: str, client: Client) -> None:
+                           dataset: str, bigquery_service: BigQueryService) -> None:
     fetch_entities_query = f"""
         SELECT {entity_type}_id
         FROM `{bq_project}.{dataset}.{entity_type}` WHERE {entity_type}_id IN
@@ -34,19 +38,17 @@ def verify_entities_loaded(entity_type: MetadataType, expected_entities: list[Me
     query_params = [
         ArrayQueryParameter("entity_ids", "STRING", expected_ids)
     ]
-    job_config = QueryJobConfig()
-    job_config.query_parameters = query_params
 
-    loaded_ids = {row[f'{entity_type}_id'] for row in client.query(
-        fetch_entities_query, project=bq_project, job_config=job_config
+    loaded_ids = {row[f'{entity_type}_id'] for row in bigquery_service.run_query(
+        fetch_entities_query, bigquery_project=bq_project, location='US', query_params=query_params
     )}
 
     set_diff = expected_ids - loaded_ids
     assert len(set_diff) == 0, f"Not all expected IDs found [diff = {set_diff}]"
 
 
-def run(bq_project: str, dataset: str, snapshot: bool, project_id: Optional[str]) -> None:
-    client = Client(project=bq_project)
+def run(bq_project: str, dataset: str, snapshot: bool, project_id: str) -> None:
+    bigquery_service = BigQueryService(Client(project=bq_project))
     if not snapshot:
         dataset = f"datarepo_{dataset}"
 
@@ -58,8 +60,8 @@ def run(bq_project: str, dataset: str, snapshot: bool, project_id: Optional[str]
     if project_id:
         query = query + f"""  WHERE project_id = '{project_id}'"""
 
-    links_rows = [row for row in client.query(query).result()]
-    verify_all_subgraphs_in_dataset(links_rows, bq_project, dataset, client)
+    links_rows = [row for row in bigquery_service.run_query(query, bq_project, 'US')]
+    verify_all_subgraphs_in_dataset(links_rows, bq_project, dataset, bigquery_service)
 
 
 if __name__ == '__main__':
--- orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
+++ orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
@@ -1,5 +1,8 @@
 from unittest.mock import MagicMock, patch, Mock
 
+from dagster import ResourceDefinition
+
+from hca_orchestration.contrib.bigquery import BigQueryService
 from hca_orchestration.pipelines.copy_project import copy_project
 from hca_orchestration.models.hca_dataset import TdrDataset
 from hca_orchestration.resources.hca_project_config import HcaProjectCopyingConfig
@@ -12,13 +15,17 @@ from hca_orchestration.resources.hca_project_config import HcaProjectCopyingConf
 @patch("hca_manage.bq_managers.DuplicatesManager.get_all_table_names", return_value=set())
 @patch("hca_manage.bq_managers.CountsManager.get_rows", return_value=set())
 def test_copy_project(*mocks) -> None:
+    bq_service = MagicMock(spec=BigQueryService)
+    bq_service.get_projects_in_dataset = MagicMock(return_value={"fake_source_project_id"})
+    bq_service.get_links_in_dataset = MagicMock(return_value=[])
+    bq_service.bigquery_client = MagicMock()
     result = copy_project.execute_in_process(
         resources={
             "bigquery_client": MagicMock(),
             "data_repo_client": MagicMock(),
             "gcs": MagicMock(),
             "scratch_config": MagicMock(),
-            "bigquery_service": MagicMock(),
+            "bigquery_service": ResourceDefinition.hardcoded_resource(bq_service),
             "hca_project_copying_config": HcaProjectCopyingConfig("fake_source_project_id", "fake_source_snapshot_name", "fake_bq_project_id", "fake_region"),
             "target_hca_dataset": TdrDataset("fake_name", "fake_id", "fake_gcp_project_id", "fake_billing_profile_id", "us-fake-region"),
             "load_tag": MagicMock(),
