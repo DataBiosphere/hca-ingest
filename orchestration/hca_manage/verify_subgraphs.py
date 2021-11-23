d2d0e45318674d19bcd34a49435f2611875b32b2
Drew Herbst <aherbst@broadinstitute.org>
2021-11-23 08:58:44 -0500

    Add subgraph verification step

--- orchestration/hca_manage/verify_subgraphs.py
+++ orchestration/hca_manage/verify_subgraphs.py
@@ -4,14 +4,18 @@ Verifies that all nodes in the subgraphs of a dataset or snapshot are loaded
 import argparse
 from typing import Optional
 
-from google.cloud.bigquery import Client, ArrayQueryParameter, QueryJobConfig, Row
+from google.cloud.bigquery import Client, ArrayQueryParameter
+from google.cloud.bigquery.table import RowIterator
 
+from hca_orchestration.contrib.bigquery import BigQueryService
+from hca_manage.common import data_repo_host, get_api_client
+from hca_orchestration.contrib.data_repo.data_repo_service import DataRepoService
 from hca_orchestration.models.entities import MetadataEntity, build_subgraph_from_links_row
 from hca_orchestration.support.subgraphs import build_subgraph_nodes
 from hca_orchestration.support.typing import MetadataType
 
 
-def verify_all_subgraphs_in_dataset(links_rows: list[Row], bq_project: str, dataset: str, client: Client) -> None:
+def verify_all_subgraphs_in_dataset(links_rows: RowIterator, bq_project: str, dataset: str, client: Client) -> None:
     links = [
         build_subgraph_from_links_row(row) for row in links_rows
     ]
@@ -23,7 +27,7 @@ def verify_all_subgraphs_in_dataset(links_rows: list[Row], bq_project: str, data
 
 
 def verify_entities_loaded(entity_type: MetadataType, expected_entities: list[MetadataEntity], bq_project: str,
-                           dataset: str, client: Client) -> None:
+                           dataset: str, bigquery_service: BigQueryService) -> None:
     fetch_entities_query = f"""
         SELECT {entity_type}_id
         FROM `{bq_project}.{dataset}.{entity_type}` WHERE {entity_type}_id IN
@@ -34,11 +38,9 @@ def verify_entities_loaded(entity_type: MetadataType, expected_entities: list[Me
     query_params = [
         ArrayQueryParameter("entity_ids", "STRING", expected_ids)
     ]
-    job_config = QueryJobConfig()
-    job_config.query_parameters = query_params
 
-    loaded_ids = {row[f'{entity_type}_id'] for row in client.query(
-        fetch_entities_query, project=bq_project, job_config=job_config
+    loaded_ids = {row[f'{entity_type}_id'] for row in bigquery_service.run_query(
+        fetch_entities_query, bigquery_project=bq_project, location='US', query_params=query_params
     )}
 
     set_diff = expected_ids - loaded_ids
@@ -46,7 +48,22 @@ def verify_entities_loaded(entity_type: MetadataType, expected_entities: list[Me
 
 
 def run(bq_project: str, dataset: str, snapshot: bool, project_id: Optional[str]) -> None:
-    client = Client(project=bq_project)
+    real_prod_dataset = False
+    if not bq_project and not dataset and project_id:
+        print("No bq project or dataset provided, assuming this is in real_prod and looking for project-specific dataset...")
+        host = data_repo_host["real_prod"]
+        data_repo_client = get_api_client(host=host)
+        data_repo_service = DataRepoService(data_repo_client)
+        tdr_dataset = data_repo_service.find_dataset(project_id)
+        if not tdr_dataset:
+            raise ValueError(f"Couldn't find dataset for query {dataset}")
+
+        bq_project = tdr_dataset.project_id
+        dataset = tdr_dataset.dataset_name
+        real_prod_dataset = True
+        print("Found dataset")
+
+    bigquery_service = BigQueryService(Client(project=bq_project))
     if not snapshot:
         dataset = f"datarepo_{dataset}"
 
@@ -55,17 +72,25 @@ def run(bq_project: str, dataset: str, snapshot: bool, project_id: Optional[str]
     SELECT * FROM `{bq_project}.{dataset}.links`
     """
 
-    if project_id:
+    if project_id and not real_prod_dataset:
         query = query + f"""  WHERE project_id = '{project_id}'"""
 
-    links_rows = [row for row in client.query(query).result()]
-    verify_all_subgraphs_in_dataset(links_rows, bq_project, dataset, client)
+    links_rows = [row for row in bigquery_service.run_query(query, bq_project, 'US')]
+
+    if real_prod_dataset:
+        projects_found = set()
+        for row in links_rows:
+            projects_found.add(row["project_id"])
+        print(f"Projects in dataset = {projects_found}")
+        assert len(projects_found) == 1, "Should only be one project in real_prod datasets"
+
+    verify_all_subgraphs_in_dataset(links_rows, bq_project, dataset, bigquery_service)
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("-b", "--bq-project", required=True)
-    argparser.add_argument("-d", "--dataset", required=True)
+    argparser.add_argument("-b", "--bq-project")
+    argparser.add_argument("-d", "--dataset")
     argparser.add_argument("-s", "--snapshot", action="store_true")
     argparser.add_argument("-p", "--project_id")
     args = argparser.parse_args()
--- orchestration/hca_orchestration/solids/validate_dataset.py
+++ orchestration/hca_orchestration/solids/validate_dataset.py
@@ -3,9 +3,45 @@ from typing import Iterator
 from dagster import op, Failure, In, Nothing, AssetMaterialization, AssetKey, Out, Output
 from dagster.core.execution.context.compute import AbstractComputeExecutionContext
 
+from hca_manage.common import ProblemCount
+from hca_orchestration.contrib.bigquery import BigQueryService
 from hca_manage.check import CheckManager
 from hca_orchestration.models.hca_dataset import TdrDataset
 from hca_orchestration.resources.hca_project_config import HcaProjectCopyingConfig
+from hca_manage.verify_subgraphs import verify_all_subgraphs_in_dataset
+
+
+@op(
+    required_resource_keys={
+        "target_hca_dataset",
+        "bigquery_service",
+        "bigquery_client",
+        "hca_project_copying_config"
+    }
+)
+def verify_subgraphs(context: AbstractComputeExecutionContext, result: ProblemCount) -> None:
+    bigquery_service: BigQueryService = context.resources.bigquery_service
+    target_hca_dataset: TdrDataset = context.resources.target_hca_dataset
+    project_copying_config: HcaProjectCopyingConfig = context.resources.hca_project_copying_config
+
+    projects_found = bigquery_service.get_projects_in_dataset(
+        target_hca_dataset.dataset_name,
+        target_hca_dataset.project_id,
+        target_hca_dataset.bq_location
+    )
+
+    if projects_found != {project_copying_config.source_hca_project_id}:
+        raise Failure(
+            f"Incorrect projects present in dataset {target_hca_dataset.dataset_name}, should be {project_copying_config.source_hca_project_id}, found {projects_found}")
+
+    links_rows = bigquery_service.get_links_in_dataset(target_hca_dataset.dataset_name,
+                                                       target_hca_dataset.project_id,
+                                                       target_hca_dataset.bq_location)
+    verify_all_subgraphs_in_dataset(
+        links_rows,
+        target_hca_dataset.project_id,
+        target_hca_dataset.dataset_name,
+        context.resources.bigquery_client)
 
 
 @op(
