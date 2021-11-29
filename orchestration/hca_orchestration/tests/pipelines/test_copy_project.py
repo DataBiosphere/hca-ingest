7e0aa77fb44724b3cd074e933b826c0b0e2687f5
Drew Herbst <aherbst@broadinstitute.org>
2021-11-29 10:45:40 -0500

    Fix test

--- orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
+++ orchestration/hca_orchestration/tests/pipelines/test_copy_project.py
@@ -49,16 +49,15 @@ def test_copy_project_fails_on_file_load_failure() -> None:
         "failedFiles": 1
     })
 
-    with pytest.raises(Failure, match=)
-    result = copy_project.execute_in_process(
-        resources={
-            "bigquery_client": MagicMock(),
-            "data_repo_client": ResourceDefinition.hardcoded_resource(data_repo_client),
-            "gcs": MagicMock(),
-            "scratch_config": MagicMock(),
-            "bigquery_service": ResourceDefinition.hardcoded_resource(bq_service),
-            "hca_project_copying_config": HcaProjectCopyingConfig("fake_source_project_id", "fake_source_snapshot_name", "fake_bq_project_id", "fake_region"),
-            "target_hca_dataset": TdrDataset("fake_name", "fake_id", "fake_gcp_project_id", "fake_billing_profile_id", "us-fake-region"),
-            "load_tag": MagicMock(),
-        })
-    assert result.success
+    with pytest.raises(Failure, match="had failedFiles = "):
+        copy_project.execute_in_process(
+            resources={
+                "bigquery_client": MagicMock(),
+                "data_repo_client": ResourceDefinition.hardcoded_resource(data_repo_client),
+                "gcs": MagicMock(),
+                "scratch_config": MagicMock(),
+                "bigquery_service": ResourceDefinition.hardcoded_resource(bq_service),
+                "hca_project_copying_config": HcaProjectCopyingConfig("fake_source_project_id", "fake_source_snapshot_name", "fake_bq_project_id", "fake_region"),
+                "target_hca_dataset": TdrDataset("fake_name", "fake_id", "fake_gcp_project_id", "fake_billing_profile_id", "us-fake-region"),
+                "load_tag": MagicMock(),
+            })
