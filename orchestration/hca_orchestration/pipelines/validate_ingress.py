fd6a5f1058ae3ad802904a2011b02f87ea47fe81
bahill <bhill@broadinstitute.org>
2022-08-05 10:55:58 -0400

    updating for Python Black linter suggestions

--- orchestration/hca_orchestration/pipelines/validate_ingress.py
+++ orchestration/hca_orchestration/pipelines/validate_ingress.py
@@ -1,19 +1,25 @@
-from dagster import failure_hook, HookContext, graph, resource, InitResourceContext, Partition
+from dagster import (
+    failure_hook,
+    HookContext,
+    graph,
+    resource,
+    InitResourceContext,
+    Partition,
+)
 from dagster_utils.typing import DagsterObjectConfigSchema
 
 from hca_manage.validation import HcaValidator
-from hca_orchestration.solids.validate_ingress import pre_flight_validate, notify_slack_of_successful_ingress_validation
+from hca_orchestration.solids.validate_ingress import (
+    pre_flight_validate,
+    notify_slack_of_successful_ingress_validation,
+)
 
 
-def run_config_for_validation_ingress_partition(partition: Partition) -> DagsterObjectConfigSchema:
+def run_config_for_validation_ingress_partition(
+        partition: Partition,
+) -> DagsterObjectConfigSchema:
     return {
-        "solids": {
-            "pre_flight_validate": {
-                "config": {
-                    "staging_area": partition.value
-                }
-            }
-        }
+        "solids": {"pre_flight_validate": {"config": {"staging_area": partition.value}}}
     }
 
 
@@ -22,13 +28,13 @@ def staging_area_validator(init_context: InitResourceContext) -> HcaValidator:
     return HcaValidator()
 
 
-@failure_hook(
-    required_resource_keys={'slack', 'dagit_config'}
-)
+@failure_hook(required_resource_keys={'slack', 'dagit_config'})
 def validation_failed_notification(context: HookContext) -> None:
-    lines = ("Validation Ingress Failed",
-             "Dagit link: " + f"<{context.resources.dagit_config.run_url(context.run_id)}|View in Dagit>"
-             )
+    lines = (
+        "Validation Ingress Failed",
+        "Dagit link: "
+        + f"<{context.resources.dagit_config.run_url(context.run_id)}|View in Dagit>",
+    )
     slack_msg_text = "\n".join(lines)
     context.resources.slack.send_message(text=slack_msg_text)
 
