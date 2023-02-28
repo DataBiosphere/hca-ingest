cd9b042a268ce14bf8d9b3236579928ece4e2e7e
bahill <bhill@broadinstitute.org>
2023-02-28 15:33:26 -0500

    appeasing the linter gods -r it is. https://askubuntu.com/questions/1429809/read-without-r-will-mangle-backslashes

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -10,7 +10,7 @@ echo "using build tag: ${BUILD_TAG}"
 if docker manifest inspect $BUILD_TAG > /dev/null ;
 then
     echo "Docker image already exists. Do you want to overwrite this tag?."
-    read -p "Press [Enter] to overwrite or [Ctrl+C] to exit and change the version number."
+    read -r -p "Press [Enter] to overwrite or [Ctrl+C] to exit and change the version number."
 fi
 
 echo "Building and pushing new image ${BUILD_TAG}"
