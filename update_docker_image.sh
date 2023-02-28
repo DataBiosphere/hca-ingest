fc4e45310787af8aaa68c3f1fbc524a7fd1c9b67
bahill <bhill@broadinstitute.org>
2023-02-28 14:18:23 -0500

    Adding a script to build and push an updated Docker image

---
+++ update_docker_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# DEVELOPER: update this field anytime you make a new docker image
+VERSION="0.4"
+BUILD_TAG="us-east4-docker.pkg.dev/broad-dsp-monster-hca-dev/monster-dev-env/hca_ingest_compose_dev_env:${VERSION}"
+
+echo "using build tag: ${BUILD_TAG}"
+
+if docker manifest inspect $BUILD_TAG > /dev/null ;
+then
+    echo "Docker image already exists. Do you want to overwrite this tag?."
+    read -p "Press [Enter] to overwrite or [Ctrl+C] to exit and change the version number."
+fi
+
+echo "Building and pushing new image ${BUILD_TAG}"
+
+#docker build . -t ${BUILD_TAG}
+#docker push "${BUILD_TAG}"
