4a9debb66c43b1b932a4b6129a1eee8d58022813
bahill <bhill@broadinstitute.org>
2023-08-01 13:18:53 -0400

    Tell the linter to ignore that this is gross and not DRY

--- orchestration/hca_orchestration/config/prod_migration/prod_migration.py
+++ orchestration/hca_orchestration/config/prod_migration/prod_migration.py
@@ -70,17 +70,17 @@ def run_config_per_project_snapshot_job_dev(partition: Partition) -> DagsterObje
 
 # for dev releases - uses dev config
 # Definitely hacky and very not dry - but this is all going away, and it's how it was done before
-# jsdcp:ignore-start
 def run_config_per_project_snapshot_job_dev(partition: Partition) -> DagsterObjectConfigSchema:
     path = file_relative_path(
         __file__, os.path.join("./run_config", "per_project_snapshot_dev.yaml")
     )
+    # jsdcp:ignore-start
     run_config: DagsterObjectConfigSchema = load_yaml_from_path(path)
 
     # we bake the release tag into the uploaded partitions csv (i.e, <uuid>,<release tag>)
     project_id, release_tag = partition.value.split(',')
     run_config["resources"]["snapshot_config"]["config"]["source_hca_project_id"] = project_id
     run_config["resources"]["snapshot_config"]["config"]["qualifier"] = release_tag
+    # jscpd:ignore-end
 
     return run_config
-# jsdcp:ignore-end
