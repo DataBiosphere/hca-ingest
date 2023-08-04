1399d7c1a02dc50c80064c77ab3fd33ec015c181
bahill <bhill@broadinstitute.org>
2023-08-04 16:38:23 -0400

    fixed path to make_public partition config

--- orchestration/hca_orchestration/config/dcp_release/dcp_release.py
+++ orchestration/hca_orchestration/config/dcp_release/dcp_release.py
@@ -77,11 +77,10 @@ def dev_run_config_for_dcp_release_per_project_partition(partition: Partition) -
 # run config for make_snapshot_public_job
 def run_config_per_project_public_snapshot_job(partition: Partition) -> DagsterObjectConfigSchema:
     path = file_relative_path(
-        __file__, os.path.join("./run_config", "per_project_public_snapshot.yaml")
+        __file__, os.path.join("./run_config/prod", "per_project_public_snapshot.yaml")
     )
     # jsdcp:ignore-start
     run_config: DagsterObjectConfigSchema = load_yaml_from_path(path)
-
     # we bake the release tag into the uploaded partitions csv (i.e, <uuid>,<release tag>)
     project_id, release_tag = partition.value.split(',')
     run_config["resources"]["snapshot_config"]["config"]["source_hca_project_id"] = project_id
