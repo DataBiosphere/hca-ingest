f6aa32b3f96eccde629a0faf87516fe0e1826a8e
Sam Novod <samn@broadinstitute.org>
2023-09-29 14:41:29 -0400

    Update deduplicate_staging_areas to take check specific file exists and take in extra args

--- orchestration/hca_manage/deduplicate_staging_areas.py
+++ orchestration/hca_manage/deduplicate_staging_areas.py
@@ -7,8 +7,24 @@ Usage:
 import os
 import re
 import argparse
+import sys
 from google.cloud import storage
 import pandas as pd
+
+STAGING_AREA_BUCKETS = {
+    "prod": {
+        "EBI": "gs://broad-dsp-monster-hca-prod-ebi-storage/prod",
+        "UCSC": "gs://broad-dsp-monster-hca-prod-ebi-storage/prod",
+        "LANTERN": "gs://broad-dsp-monster-hca-prod-lantern",
+        "LATTICE": "gs://broad-dsp-monster-hca-prod-lattice/staging"
+    },
+    "dev": {
+        "EBI": "gs://broad-dsp-monster-hca-dev-ebi-staging/dev",
+        "UCSC": "gs://broad-dsp-monster-hca-dev-ebi-staging/dev",
+    }
+}
+
+#broad-dsp-monster-hca-dev-ebi-staging/dev/003d5674-9bf6-4e51-ab1b-8fed80c308b9
 
 # Function to return the objects in a staging area bucket
 def get_staging_area_objects(bucket_name, prefix, delimiter=None):
@@ -74,24 +90,61 @@ def batch_delete_files(delete_list, bucket_name, prefix, delimiter=None):
         except Exception as e:
             print(f"Error deleting objects: {str(e)}")
 
+def create_staging_area_json(bucket_name, staging_area_json_prefix):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(staging_area_json_prefix)
+    print(f"Creating {os.path.join('gs://', bucket_name, staging_area_json_prefix)}")
+    with blob.open("w") as f:
+        f.write('{"is_delta": false}')
+
+# Get staging area
+def get_staging_area(staging_area, institution, environment, uuid):
+    # If institution is provided then infer staging area fom that and uuid
+    if institution:
+        # Confirm uuid is passed in if --institution is used
+        if not uuid:
+            print("Must provide --uuid if using --institution")
+            sys.exit(1)
+        return os.path.join(STAGING_AREA_BUCKETS[environment][institution], uuid)
+    return staging_area
+
+def check_staging_area_json_exists(bucket, prefix):
+    storage_client = storage.Client()
+    gcs_bucket = storage_client.bucket(bucket)
+    return gcs_bucket.get_blob(prefix)
+
 #  Main function
 if __name__ == "__main__":
 
     # Set up argument parser
-    parser = argparse.ArgumentParser(description="Remove outdated entity files from HCA staging area.")
-    parser.add_argument("-s", "--staging_area", required=True, type=str, help="Full GCS path to the staging area of interest.")
+    parser = argparse.ArgumentParser(description="Remove outdated entity files from HCA staging area and check if staging_area.json exists.")
+    path_parser = parser.add_mutually_exclusive_group(required=True)
+    path_parser.add_argument("-s", "--staging_area", type=str, help="Full GCS path to the staging area of interest.")
+    path_parser.add_argument("-i", "--institution", choices=['EBI', 'LATTICE', 'UCSC'], type=str, help="Must provide uuid if using institution")
+    parser.add_argument('-e', '--env', choices=['prod', 'dev'], default='prod')
+    parser.add_argument("-u", "--uuid", help="Only used if using --institution instead of --staging_area")
     parser.add_argument("-p", "--print_files", required=False, action="store_true", help="Add argument to print files to be removed.", default=False)
     parser.add_argument("-n", "--skip_deletion", required=False, action="store_true", help="Add argument to skip file deltion.", default=False)
     args = parser.parse_args()
 
+    staging_area = get_staging_area(args.staging_area, args.institution, args.env, args.uuid)
+
     # Initialize variables
-    bucket_name = re.match("gs:\/\/([a-z0-9\-_]+)\/", args.staging_area).group(1)
-    prefix = re.match("gs:\/\/[a-z0-9\-_]+\/([A-Za-z0-9\-_\/\.]+)", args.staging_area).group(1)
+    bucket_name = re.match("gs:\/\/([a-z0-9\-_]+)\/", staging_area).group(1)
+    prefix = re.match("gs:\/\/[a-z0-9\-_]+\/([A-Za-z0-9\-_\/\.]+)", staging_area).group(1)
     if prefix[-1] != "/":
         prefix += "/"
 
+    # Check if staging_area.json exists
+    staging_area_prefix = os.path.join(prefix, 'staging_area.json')
+    if not check_staging_area_json_exists(bucket_name, staging_area_prefix):
+        print(f"staging_area.json does not exist in {staging_area}")
+        # Upload if it does not exist
+        create_staging_area_json(bucket_name, staging_area_prefix)
+
     # Call functions to identify and remove outdated entity files
-    print(f"Evaluating outdated files in staging area: {args.staging_area}")
+    print(f"Evaluating outdated files in staging area: {staging_area}")
     objects_list = get_staging_area_objects(bucket_name, prefix)
     print(f"\t- Total objects found: {len(objects_list)}")
     delete_list = identify_outdated_files(objects_list)
