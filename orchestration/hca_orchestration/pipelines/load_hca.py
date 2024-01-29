3de4055d0ca6e9accee79392d24785441890aaa2
bahill <bhill@broadinstitute.org>
2024-01-29 14:40:43 -0500

    linting

--- orchestration/hca_orchestration/pipelines/load_hca.py
+++ orchestration/hca_orchestration/pipelines/load_hca.py
@@ -8,7 +8,6 @@ import warnings
 
 import sentry_sdk
 from dagster import ExperimentalWarning, graph
-
 from hca_orchestration.solids.load_hca.data_files.load_data_files import (
     import_data_files,
 )
@@ -20,11 +19,11 @@ from hca_orchestration.solids.load_hca.non_file_metadata.load_non_file_metadata 
 )
 from hca_orchestration.solids.load_hca.stage_data import (
     clear_scratch_dir,
-    create_scratch_dataset, 
+    create_scratch_dataset,
     pre_process_metadata,
 )
 from hca_orchestration.solids.load_hca.utilities import (
-    send_start_notification, 
+    send_start_notification,
     validate_and_send_finish_notification,
 )
 
@@ -37,6 +36,7 @@ SENTRY_DSN = os.getenv(
 )
 if SENTRY_DSN:
     sentry_sdk.init(dsn=SENTRY_DSN, traces_sample_rate=1.0)
+
 
 @graph
 def load_hca() -> None:
