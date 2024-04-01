a6dd14142ce3b9a6217d7a43d62e2448be120912
bahill <bhill@broadinstitute.org>
2024-04-01 11:30:50 -0400

    first attempt to add sleep to load_hca to see if the e2e issue is time sensitive.

--- orchestration/hca_orchestration/tests/e2e/test_load_hca.py
+++ orchestration/hca_orchestration/tests/e2e/test_load_hca.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from dagster import execute_pipeline
 
 from hca_orchestration.repositories.local_repository import load_hca_job
@@ -12,6 +13,8 @@ def test_load_hca(load_hca_run_config, dataset_name, tdr_bigquery_client, datase
         job,
         run_config=load_hca_run_config
     )
+
+    time.sleep(600)  # pausing execution to allow for permissions to propagate
 
     bq_project = dataset_info.dataset_data_project_id
     assert_metadata_loaded("analysis_file", dataset_name, bq_project, tdr_bigquery_client)
